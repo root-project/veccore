7152c6f6cbc753ba18c62153684d3d1e1abc5952
Calebe de Paula Bianchini <bianchini@ncc.unesp.br>
2015-06-04 13:48:37 -0500

    Revert "Merge branch 'master' into calebe/offload"
    This reverts commit af77703d62a02143bcd5b426c2a428652124ca54, reversing
    changes made to a5f000a46d05678f86057210dcce45092b3da20c.

--- inc/VariableSizeObj.h
+++ inc/VariableSizeObj.h
@@ -4,10 +4,6 @@
 #ifndef VECCORE_VARIABLESIZEOBJ_H
 #define VECCORE_VARIABLESIZEOBJ_H
 
-#ifdef OFFLOAD_MODE
-#pragma offload_attribute(push, target(mic))
-#endif
-
 // This file will eventually move in VecCore.
 #include "base/Global.h"
 
@@ -169,9 +165,5 @@ namespace VecCore {
    };
 
 }
-
-#ifdef OFFLOAD_MODE
-#pragma offload_attribute(pop)
-#endif
 
 #endif
