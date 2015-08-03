096b1090dfeba605b596feda677c822c15433ce2
Guilherme Amadio <amadio@ncc.unesp.br>
2015-08-03 15:24:07 +0200

    Update new backend test

--- test/new_backend_test.cc
+++ test/new_backend_test.cc
@@ -24,49 +24,50 @@ template <class Backend> void test_backend()
     printf("Real_v::Size       == %d\n\n", Real_v::Size);
     printf("Real_v::Mask::Size == %d\n\n", Real_v::Size);
 
-    Real_v x(18.0);
+    Real_v x(0.0);
 
     for (int i = 0; i < Real_v::Size; i++)
-	    x[i] = i*i;
+	    x[i] = i;
 
-    x = cos(x);
-
-    Mask_t mask = x >= Real_v(2.0);
+    Mask_t mask = x > Real_v(Real_v::Size/2.0);
 
     Real_t *xptr = (Real_t*)(&x);
 
     for (int i = 0; i < Real_v::Size; i++)
-	    printf("x[%d] = %f, x[i] >= 2.0 == %s\n",
-	        i, xptr[i], mask[i] ? "true" : "false");
+	    printf("x[%d] = %.1f, x[i] > %d == %s\n",
+	        i, xptr[i], Real_v::Size/2, mask[i] ? "true" : "false");
 
-    for (int i = 0; i < Real_v::Size; i++) x[i] = i*i*i;
-
-    for (int i = 0; i < Real_v::Size; i++)
-	    printf("x[%d] = %f, x[i] >= 2.0 == %s\n",
-	        i, xptr[i], mask[i] ? "true" : "false");
-
-    mask = x < Real_v(4.0);
+    printf("\n");
 
     for (int i = 0; i < Real_v::Size; i++)
-	    printf("x[%d] = %f, x[i] < 4.0 == %s\n",
-	        i, xptr[i], mask[i] ? "true" : "false");
+	    x[i] = Real_v::Size - i;
 
-    x(mask) = Real_v(17.0);
+    mask = x > Real_v(Real_v::Size/2.0);
 
     for (int i = 0; i < Real_v::Size; i++)
-	    printf("x[%d] = %f, mask[i] == %s\n",
-	        i, xptr[i], mask[i] ? "true" : "false");
+	    printf("x[%d] = %.1f, x[i] > %d == %s\n",
+	        i, xptr[i], Real_v::Size/2, mask[i] ? "true" : "false");
 }
 
 int main(int argc, char *argv[])
 {
 
-    printf("Scalar Backend Test:\n\n");
+    printf("Scalar Backend Test (float):\n\n");
+    test_backend<VecCore::Backend::Scalar<float>>();
+
+    printf("\n\n");
+
+    printf("Scalar Backend Test (double):\n\n");
     test_backend<VecCore::Backend::Scalar<double>>();
 
     printf("\n\n");
 
-    printf("Vector Backend Test:\n\n");
+    printf("Vector Backend Test (float):\n\n");
+    test_backend<VecCore::Backend::Vector<float>>();
+
+    printf("\n\n");
+
+    printf("Vector Backend Test (double):\n\n");
     test_backend<VecCore::Backend::Vector<double>>();
 
     return 0;
