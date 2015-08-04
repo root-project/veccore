ffb075cf91bc89971eb149d3438228ceddfa08ee
Guilherme Amadio <amadio@ncc.unesp.br>
2015-08-04 13:06:36 +0200

    Add inline to VECCORE_FORCE_INLINE macro

--- core/base.h
+++ core/base.h
@@ -6,7 +6,7 @@
 
 #ifndef VECCORE_NO_FORCED_INLINING
 #    define VECCORE_FORCE_NOINLINE __attribute__((noinline))
-#    define VECCORE_FORCE_INLINE   __attribute__((always_inline))
+#    define VECCORE_FORCE_INLINE   inline __attribute__((always_inline))
 #else
 #    define VECCORE_FORCE_NOINLINE
 #    define VECCORE_FORCE_INLINE
