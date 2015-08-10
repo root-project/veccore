3c1d18189e8240ee125d235fecd864d3af3091a8
Guilherme Amadio <amadio@ncc.unesp.br>
2015-08-10 12:05:29 +0200

    Fix formatting with clang-format

--- test/geometry.cc
+++ test/geometry.cc
@@ -20,88 +20,133 @@
 
 using namespace VecCore::Math;
 
-template <class Backend> void test()
-{
-    typedef typename Backend::Real_t Real_t;
-    typedef typename Backend::Real_v Real_v;
-    typedef typename Backend::Real_v::Mask Mask_t;
+template <class Backend> void test() {
+  typedef typename Backend::Real_t Real_t;
+  typedef typename Backend::Real_v Real_v;
+  typedef typename Backend::Real_v::Mask Mask_t;
 
-    // test points and vectors (scalar test)
-    
-    // constructors
+  // test points and vectors (scalar test)
 
-    Point3D<Real_t>  px(1.0, 0.0, 0.0);
-    Point3D<Real_t>  py(0.0, 1.0, 0.0);
-    Point3D<Real_t>  pz(0.0, 0.0, 1.0);
+  // constructors
 
-    Vector3D<Real_t> vx(1.0, 0.0, 0.0);
-    Vector3D<Real_t> vy(0.0, 1.0, 0.0);
-    Vector3D<Real_t> vz(0.0, 0.0, 1.0);
+  Point3D<Real_t> px(Real_t(1.0), Real_t(0.0), Real_t(0.0));
+  Point3D<Real_t> py(Real_t(0.0), Real_t(1.0), Real_t(0.0));
+  Point3D<Real_t> pz(Real_t(0.0), Real_t(0.0), Real_t(1.0));
 
-    Point3D<Real_t>  tmp1 = Real_t(3.0) * px + (py / Real_t(2.0)) - vz;
-    Vector3D<Real_t> tmp2 = Real_t(3.0) * px + (py / Real_t(2.0)) - pz;
+  Vector3D<Real_t> vx(Real_t(1.0), Real_t(0.0), Real_t(0.0));
+  Vector3D<Real_t> vy(Real_t(0.0), Real_t(1.0), Real_t(0.0));
+  Vector3D<Real_t> vz(Real_t(0.0), Real_t(0.0), Real_t(1.0));
 
-    // dot and cross products
+  Point3D<Real_t> tmp1 = Real_t(3.0) * px + (py / Real_t(2.0)) - vz;
+  Vector3D<Real_t> tmp2 = Real_t(3.0) * px + (py / Real_t(2.0)) - pz;
 
-    assert(abs(Dot(px, px) - 1.0) < 1e-6);
-    assert(abs(Dot(py, py) - 1.0) < 1e-6);
-    assert(abs(Dot(pz, pz) - 1.0) < 1e-6);
-                                 
-    assert(abs(Dot(px, py) - 0.0) < 1e-6);
-    assert(abs(Dot(py, pz) - 0.0) < 1e-6);
-    assert(abs(Dot(pz, px) - 0.0) < 1e-6);
-                                 
-    assert(abs(Dot(vx, vx) - 1.0) < 1e-6);
-    assert(abs(Dot(vy, vy) - 1.0) < 1e-6);
-    assert(abs(Dot(vz, vz) - 1.0) < 1e-6);
-                                 
-    assert(abs(Dot(vx, vy) - 0.0) < 1e-6);
-    assert(abs(Dot(vy, vz) - 0.0) < 1e-6);
-    assert(abs(Dot(vz, vx) - 0.0) < 1e-6);
+  // dot and cross products
 
-    assert(abs(Norm(Cross(vx, vy) - vz)) < 1e-6);
-    assert(abs(Norm(Cross(vz, vx) - vy)) < 1e-6);
-    assert(abs(Norm(Cross(vy, vz) - vx)) < 1e-6);
+  assert(abs(Dot(px, px) - 1.0) < 1e-6);
+  assert(abs(Dot(py, py) - 1.0) < 1e-6);
+  assert(abs(Dot(pz, pz) - 1.0) < 1e-6);
 
-    assert(abs(Norm<Real_t, 2>(Point3D<Real_t>(1.0, 2.0, 3.0)) - 14.0) < 1.0e-6);
-    assert(abs(Norm<Real_t, 3>(Point3D<Real_t>(1.0, 2.0, 3.0)) - pow(36.0,1.0/3.0)) < 1.0e-6);
+  assert(abs(Dot(px, py) - 0.0) < 1e-6);
+  assert(abs(Dot(py, pz) - 0.0) < 1e-6);
+  assert(abs(Dot(pz, px) - 0.0) < 1e-6);
 
-    // test points and vectors (vector test)
+  assert(abs(Dot(vx, vx) - 1.0) < 1e-6);
+  assert(abs(Dot(vy, vy) - 1.0) < 1e-6);
+  assert(abs(Dot(vz, vz) - 1.0) < 1e-6);
 
-    // constructors
+  assert(abs(Dot(vx, vy) - 0.0) < 1e-6);
+  assert(abs(Dot(vy, vz) - 0.0) < 1e-6);
+  assert(abs(Dot(vz, vx) - 0.0) < 1e-6);
 
-    Point3D<Real_v>  px_v(1.0, 0.0, 0.0);
-    Point3D<Real_v>  py_v(0.0, 1.0, 0.0);
-    Point3D<Real_v>  pz_v(0.0, 0.0, 1.0);
+  assert(abs(Norm(Cross(vx, vy) - vz)) < 1e-6);
+  assert(abs(Norm(Cross(vz, vx) - vy)) < 1e-6);
+  assert(abs(Norm(Cross(vy, vz) - vx)) < 1e-6);
 
-    Vector3D<Real_v> vx_v(1.0, 0.0, 0.0);
-    Vector3D<Real_v> vy_v(0.0, 1.0, 0.0);
-    Vector3D<Real_v> vz_v(0.0, 0.0, 1.0);
+  assert(abs(Norm<Real_t, 2>(Point3D<Real_t>(1.0, 2.0, 3.0)) - 14.0) < 1.0e-6);
+  assert(abs(Norm<Real_t, 3>(Point3D<Real_t>(1.0, 2.0, 3.0)) - pow(36.0, 1.0 / 3.0)) < 1.0e-6);
 
-    Mask_t m(false);
-    
-    m = abs(Dot(px, px) - 1.0) < 1e-6;
-    
-    assert(m.isFull());
+  // test points and vectors (vector test)
+
+  // constructors
+
+  Point3D<Real_v> px_v(Real_v(1.0), Real_v(0.0), Real_v(0.0));
+  Point3D<Real_v> py_v(Real_v(0.0), Real_v(1.0), Real_v(0.0));
+  Point3D<Real_v> pz_v(Real_v(0.0), Real_v(0.0), Real_v(1.0));
+
+  Vector3D<Real_v> vx_v(Real_v(1.0), Real_v(0.0), Real_v(0.0));
+  Vector3D<Real_v> vy_v(Real_v(0.0), Real_v(1.0), Real_v(0.0));
+  Vector3D<Real_v> vz_v(Real_v(0.0), Real_v(0.0), Real_v(1.0));
+
+  Mask_t m(false);
+
+  m = abs(Dot(px_v, px_v) - Real_v(1.0)) < Real_v(1e-6);
+
+  assert(m.isFull());
+
+  // test quaternion
+
+  Quaternion<Real_t> qi(1.0, 0.0, 0.0, 0.0);
+  Quaternion<Real_t> qj(0.0, 1.0, 0.0, 0.0);
+  Quaternion<Real_t> qk(0.0, 0.0, 1.0, 0.0);
+
+  Quaternion<Real_t> q1, q2, q3, q4, q5;
+
+#if 1
+#define PRINT(name, q) printf("%s [% .2f, % .2f, % .2f, % .2g]\n", #name, q[0], q[1], q[2], q[3])
+
+    q1 = Cross(qi, qj); PRINT(ij, q1);
+    q5 = Cross(qj, qi); PRINT(ji, q5);
+    q2 = Cross(qj, qk); PRINT(jk, q2);
+    q3 = Cross(qk, qi); PRINT(ki, q3);
+    q4 = Cross(qk, qk); PRINT(kk, q4);
+
+#undef PRINT
+#endif
+
+  Real_t angle = 30.0 * 3.1415926535 / 180.0;
+
+  Vector3D<Real_t> axis(Real_t(0.0), Real_t(0.0), Real_t(1.0));
+
+  Quaternion<Real_t> orientation(axis, angle);
+
+  Point3D<Real_t> origin(Real_t(0.0), Real_t(0.0), Real_t(5.0));
+
+  Transform<Real_t, Matrix3x3> T(origin, orientation, 0.5);
+  Transform<Real_t, Matrix3x3> invT = Inverse(T);
+
+  Point3D<Real_t>  test_p;
+  Vector3D<Real_t> test_v;
+
+#if 1
+#define PRINT(name, q) printf("%s [% .4f, % .4f, % .4f]\n", #name, q[0], q[1], q[2])
+
+    test_p = T(px); PRINT(test_p, test_p);
+    test_v = T(vx); PRINT(test_v, test_v);
+
+    test_p = invT(T(px+py+pz)); PRINT(test_p, test_p);
+    test_v = invT(T(vx+vy+vz)); PRINT(test_v, test_v);
+
+#undef PRINT
+#endif
+
+
 }
 
-void test_all()
-{
-    printf("test (scalar, float):\n");
-    test<VecCore::Backend::Scalar<float>>();
-    printf("\n");
-    printf("test (scalar, double):\n");
-    test<VecCore::Backend::Scalar<double>>();
-    printf("\n");
-    printf("test (vector, float):\n");
-    test<VecCore::Backend::Vector<float>>();
-    printf("\n");
-    printf("test (vector, double):\n");
-    test<VecCore::Backend::Vector<double>>();
+void test_all() {
+  printf("test (scalar, float):\n");
+  test<VecCore::Backend::Scalar<float>>();
+  printf("\n");
+  printf("test (scalar, double):\n");
+  test<VecCore::Backend::Scalar<double>>();
+  printf("\n");
+  printf("test (vector, float):\n");
+  test<VecCore::Backend::Vector<float>>();
+  printf("\n");
+  printf("test (vector, double):\n");
+  test<VecCore::Backend::Vector<double>>();
 }
 
-int main(int argc, char *argv[])
-{
-    test_all();
-    return 0;
+int main(int argc, char *argv[]) {
+  test_all();
+  return 0;
 }
