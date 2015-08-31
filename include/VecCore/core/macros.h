a50bf365868568f004e202cc26c4a508317971a7
Guilherme Amadio <amadio@ncc.unesp.br>
2015-08-31 17:27:58 +0200

    Delete unused core/macros.h

--- include/VecCore/core/macros.h
+++
@@ -1,9 +0,0 @@
-#ifndef VECCORE_MACROS_H
-#define VECCORE_MACROS_H
-
-#ifndef VECCORE_NO_FORCED_INLINING
-#    define VECCORE_FORCE_NOINLINE __attribute__((noinline))
-#    define VECCORE_FORCE_INLINE   __attribute__((always_inline))
-#endif
-
-#endif
