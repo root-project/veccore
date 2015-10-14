43dcdff2aab54ab663f942b5d817ea110ed6b7ba
Sandro Wenzel <sandro.wenzel@cern.ch>
2015-10-14 16:08:21 +0200

    constexpr SizeOf made simpler (thanks to Philippe C.)

--- inc/VariableSizeObj.h
+++ inc/VariableSizeObj.h
@@ -160,15 +160,8 @@ namespace veccore{
 
       // Equivalent of sizeof function
       VECGEOM_CUDA_HEADER_BOTH
-      static size_t SizeOf(size_t nvalues)
+      static constexpr size_t SizeOf(size_t nvalues)
       { return (sizeof(Cont)+sizeof(V)*(nvalues-1)); }
-
-      // a templated constexpr variant of SizeOf (useful when we know nvalues at compilation time)
-      VECGEOM_CUDA_HEADER_BOTH
-      template <size_t nvalues>
-      static constexpr size_t ConstExprSizeOf(){
-        return (sizeof(Cont)+sizeof(V)*(nvalues-1));
-      }
 
    };
 
