99b996628e0b907b14faa04bdd9708be86250520
Guilherme Amadio <amadio@ncc.unesp.br>
2015-10-23 12:01:44 +0200

    Move compiler configuration into separate CMake module

---
+++ cmake/modules/CompilerSetup.cmake
@@ -0,0 +1,26 @@
+
+set(TARGET_ISA "native" CACHE STRING "Selects target instruction set architecture to support.")
+set_property(CACHE TARGET_ISA PROPERTY STRINGS native sse sse2 sse3 ssse3 sse41 sse42 avx avx2 avx512 mic)
+
+message(STATUS "Compiling for ${TARGET_ISA} SIMD instructions")
+
+if (SIMD)
+    find_package(Vc REQUIRED)
+endif()
+
+if (${CMAKE_CXX_COMPILER_ID} STREQUAL "GNU")
+    include (GCC)
+elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "Clang")
+    include (Clang)
+elseif (${CMAKE_CXX_COMPILER_ID} STREQUAL "Intel")
+    include (Intel)
+else()
+    message(WARNING "Unsupported compiler")
+endif()
+
+string(TOUPPER "FLAGS_${TARGET_ISA}" _target_isa)
+set(CMAKE_CXX_FLAGS_TARGET_ISA "${${_target_isa}}")
+
+if (CUDA)
+    include(CUDA)
+endif()
