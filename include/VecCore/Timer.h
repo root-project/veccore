106de5828bfe1020e2997974bf8c9156fc2520d6
Guilherme Amadio <amadio@ncc.unesp.br>
2015-10-23 13:19:32 +0200

    Make Timer classes a template of duration unit

--- include/VecCore/Timer.h
+++ include/VecCore/Timer.h
@@ -1,63 +1,58 @@
+#ifndef VECCORE_TIMER_H
+#define VECCORE_TIMER_H
+
 #include <chrono>
 
-class TimerInterface {
-  public:
-	virtual ~TimerInterface() {}
-    virtual void Start() = 0;
-    virtual void Reset() = 0;
-    virtual double Elapsed() = 0;
+using namespace std::chrono;
+
+template <class unit = nanoseconds>
+class Timer {
+	using clock = high_resolution_clock;
+public:
+	Timer() : fStart(), fStop() { Start(); }
+
+	void Reset() { Start(); }
+
+	void Start() { fStart = clock::now(); }
+
+	double Elapsed()
+	{
+		fStop = clock::now();
+		return duration_cast<unit>(fStop - fStart).count();
+	}
+
+private:
+	high_resolution_clock::time_point fStart, fStop;
 };
 
-template <class duration_unit = std::chrono::nanoseconds>
-class Timer : public TimerInterface {
-  public:
-    Timer() : fStart(), fStop() { Start(); }
-	~Timer() { }
+#if !defined(VECCORE_CUDA_ARCH)
 
-    inline void Reset() { Start(); }
+class cycles {};
 
-    inline void Start()
-    {
-        fStart = std::chrono::high_resolution_clock::now();
-    }
+template <> class Timer<cycles> {
+public:
+	Timer() { Start(); }
 
-    inline double Elapsed()
-    {
-        fStop = std::chrono::high_resolution_clock::now();
-        duration_unit t_elapsed =
-        std::chrono::duration_cast<duration_unit>(fStop - fStart);
-        return t_elapsed.count();
-    }
+	void Reset() { Start(); }
 
-  private:
-    std::chrono::high_resolution_clock::time_point fStart;
-    std::chrono::high_resolution_clock::time_point fStop;
+	void Start() { fStart = GetCycleCount(); }
+
+	double Elapsed()
+	{
+		return static_cast<double>(GetCycleCount() - fStart);
+	}
+
+private:
+	unsigned long long int fStart;
+
+	inline __attribute__((always_inline))
+	unsigned long long int GetCycleCount()
+	{
+		unsigned int hi, lo;
+		asm volatile("cpuid\n\t" "rdtsc" : "=a"(lo), "=d"(hi));
+		return ((unsigned long long)lo)|(((unsigned long long)hi) << 32);
+	}
 };
+#endif
 
-#if !defined(VECCORE_CUDA_DEVICE)
-class CPUCycleTimer {
-  public:
-    CPUCycleTimer() { Start(); }
-	~CPUCycleTimer() { }
-
-    inline void Reset() { Start(); }
-
-    inline void Start() { fStart = CurrentCycleCount(); }
-
-    inline double Elapsed()
-    {
-        return static_cast<double>(CurrentCycleCount() - fStart);
-    }
-
-  private:
-    unsigned long long int fStart;
-
-    inline __attribute__((always_inline))
-    unsigned long long int CurrentCycleCount()
-    {
-        unsigned int hi, lo;
-        asm volatile("cpuid\n\t" "rdtsc" : "=a"(lo), "=d"(hi));
-        return ((unsigned long long)lo)|(((unsigned long long)hi) << 32);
-    }
-};
 #endif
