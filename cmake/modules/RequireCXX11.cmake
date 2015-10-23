f483eaa739fe5632acfee98e497895c228c64bc4
Guilherme Amadio <amadio@ncc.unesp.br>
2015-10-23 11:58:29 +0200

    Fix typo in RequireCXX11.cmake

--- cmake/modules/RequireCXX11.cmake
+++ cmake/modules/RequireCXX11.cmake
@@ -1,7 +1,7 @@
 include(CheckCXXCompilerFlag)
 
-check_cxx_compiler_flag(--std=c++11 SUPPORTS_STD_CXX11)
-check_cxx_compiler_flag(--std=c++0x SUPPORTS_STD_CXX01)
+check_cxx_compiler_flag(-std=c++11 SUPPORTS_STD_CXX11)
+check_cxx_compiler_flag(-std=c++0x SUPPORTS_STD_CXX01)
 
 if(SUPPORTS_STD_CXX11)
 	set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} -std=c++11")
