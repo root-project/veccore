ff0f57245d17571cb2b98f9f71c4707574d382d2
Guilherme Amadio <amadio@ncc.unesp.br>
2015-10-23 13:06:45 +0200

    Add Backend header to include all backends

---
+++ include/VecCore/Backend.h
@@ -0,0 +1,12 @@
+#ifndef VECCORE_BACKEND_H
+#define VECCORE_BACKEND_H
+
+#include <type_traits>
+
+#include "Common.h"
+
+#include "Backend/CUDA.h"
+#include "Backend/Scalar.h"
+#include "Backend/Vector.h"
+
+#endif
