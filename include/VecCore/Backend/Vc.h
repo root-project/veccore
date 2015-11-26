32d3f98aa5f73a36c5de06b95502ed3720e9b351
Guilherme Amadio <amadio@ncc.unesp.br>
2015-11-26 10:09:27 +0100

    Pair ints and floats of same size in Vc backend

--- include/VecCore/Backend/Vc.h
+++ include/VecCore/Backend/Vc.h
@@ -8,25 +8,22 @@
 namespace vecCore {
 namespace backend {
 
-template <typename FloatT = double>
-class Vector {
+template <typename> class Vector;
+
+template <> class Vector<float> {
   public:
     static const bool EarlyExit = true;
 
-    // floating point types
-
     typedef bool                 Bool_t;
-    typedef FloatT               Real_t;
-    typedef float_t              Float_t;
-    typedef double_t             Double_t;
+    typedef float                Real_t;
+    typedef float                Float_t;
+    typedef double               Double_t;
 
     typedef Vc::Vector<Real_t>   Real_v;
     typedef Vc::Vector<Float_t>  Float_v;
     typedef Vc::Vector<Double_t> Double_v;
 
-    // integer types
-
-    typedef int                  Int_t;
+    typedef int32_t              Int_t;
     typedef int16_t              Int16_t;
     typedef int32_t              Int32_t;
     typedef int64_t              Int64_t;
@@ -36,7 +33,41 @@ class Vector {
     typedef Vc::Vector<Int32_t>  Int32_v;
     typedef Vc::Vector<Int64_t>  Int64_v;
 
-    typedef unsigned int         UInt_t;
+    typedef uint32_t             UInt_t;
+    typedef uint16_t             UInt16_t;
+    typedef uint32_t             UInt32_t;
+    typedef uint64_t             UInt64_t;
+
+    typedef Vc::Vector<UInt_t>   UInt_v;
+    typedef Vc::Vector<UInt16_t> UInt16_v;
+    typedef Vc::Vector<UInt32_t> UInt32_v;
+    typedef Vc::Vector<UInt64_t> UInt64_v;
+};
+
+template <> class Vector<double> {
+  public:
+    static const bool EarlyExit = true;
+
+    typedef bool                 Bool_t;
+    typedef double               Real_t;
+    typedef float                Float_t;
+    typedef double               Double_t;
+
+    typedef Vc::Vector<Real_t>   Real_v;
+    typedef Vc::Vector<Float_t>  Float_v;
+    typedef Vc::Vector<Double_t> Double_v;
+
+    typedef int64_t              Int_t;
+    typedef int16_t              Int16_t;
+    typedef int32_t              Int32_t;
+    typedef int64_t              Int64_t;
+
+    typedef Vc::Vector<Int_t>    Int_v;
+    typedef Vc::Vector<Int16_t>  Int16_v;
+    typedef Vc::Vector<Int32_t>  Int32_v;
+    typedef Vc::Vector<Int64_t>  Int64_v;
+
+    typedef uint64_t             UInt_t;
     typedef uint16_t             UInt16_t;
     typedef uint32_t             UInt32_t;
     typedef uint64_t             UInt64_t;
