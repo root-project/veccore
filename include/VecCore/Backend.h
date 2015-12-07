09d75cb51214afefb27e025018965bc504663c9c
Guilherme Amadio <amadio@ncc.unesp.br>
2015-12-07 11:13:12 +0100

    Move interface functions to Common.h, fix coding style issues

--- include/VecCore/Backend.h
+++ include/VecCore/Backend.h
@@ -21,54 +21,6 @@ namespace backend {
 	template <typename T> using Scalar = VcScalar<T>;
 	template <typename T> using Vector = VcVector<T>;
 #endif
-}
-
-}
-
-template <class T, class Mask>
-VECCORE_FORCE_INLINE
-void MaskedAssign(T& var, const Mask& m, const T val)
-{
-	var(m) = val;
-}
-
-template <>
-VECCORE_FORCE_INLINE
-void MaskedAssign<float, bool>(float& var, const bool& m, const float val)
-{
-    if (m) var = val;
-}
-
-template <>
-VECCORE_FORCE_INLINE
-void MaskedAssign<double, bool>(double& var, const bool& m, const double val)
-{
-    if (m) var = val;
-}
-
-template <class T, class Mask>
-VECCORE_FORCE_INLINE
-T Blend(const Mask& m, const T val1, const T val2)
-{
-    T result(val2);
-    MaskedAssign(result, m, val1);
-    return result;
-}
-
-template <class Mask>
-VECCORE_FORCE_INLINE
-bool IsFull(const Mask mask) { return mask.isFull(); }
-
-template <>
-VECCORE_FORCE_INLINE
-bool IsFull<bool>(const bool mask) { return mask; }
-
-template <class Mask>
-VECCORE_FORCE_INLINE
-bool IsEmpty(const Mask mask) { return mask.isEmpty(); }
-
-template <>
-VECCORE_FORCE_INLINE
-bool IsEmpty<bool>(const bool mask) { return !mask; }
-
+} // namespace backend
+} // namespace vecCore
 #endif
--- include/VecCore/Common.h
+++ include/VecCore/Common.h
@@ -5,14 +5,101 @@
 #include <cstdint>
 
 #ifndef VECCORE_NO_FORCED_INLINING
-#    define VECCORE_FORCE_NOINLINE __attribute__((noinline))
-#    define VECCORE_FORCE_INLINE   inline __attribute__((always_inline))
+#define VECCORE_FORCE_NOINLINE __attribute__((noinline))
+#define VECCORE_FORCE_INLINE inline __attribute__((always_inline))
 #else
-#    define VECCORE_FORCE_NOINLINE
-#    define VECCORE_FORCE_INLINE
+#define VECCORE_FORCE_NOINLINE
+#define VECCORE_FORCE_INLINE
 #endif
 
 #include "CUDA.h"
 #include "SIMD.h"
 
+namespace vecCore {
+
+// Masking Interface
+
+// IsFull
+
+template <class Mask>
+VECCORE_FORCE_INLINE
+bool IsFull(const Mask mask)
+{
+  return mask.isFull();
+}
+
+template <>
+VECCORE_FORCE_INLINE
+bool IsFull<bool>(const bool mask)
+{
+  return mask;
+}
+
+// IsEmpty
+
+template <class Mask>
+VECCORE_FORCE_INLINE
+bool IsEmpty(const Mask& mask)
+{
+  return mask.isEmpty();
+}
+
+template <>
+VECCORE_FORCE_INLINE
+bool IsEmpty<bool>(const bool& mask)
+{
+  return !mask;
+}
+
+// MaskedAssign
+
+template <typename T>
+VECCORE_FORCE_INLINE
+void MaskedAssign(bool mask, const T &val, T *const var)
+{
+  if (mask)
+    var = val;
+}
+
+template <class T, class Mask>
+VECCORE_FORCE_INLINE
+void MaskedAssign(const Mask &mask, const T &val, T *const var)
+{
+  (*var)(mask) = val;
+}
+
+// CondAssign (for backward-compatibility, deprecated by Blend below)
+
+template <typename T>
+VECCORE_FORCE_INLINE
+void CondAssign(const bool &mask, const T &tval, const T &fval, T *const var)
+{
+  (*var) = mask ? tval : fval;
+}
+
+template <class T, class Mask>
+VECCORE_FORCE_INLINE
+void CondAssign(const Mask &mask, const T &tval, const T &fval, T *const var)
+{
+  (*var) = fval;
+  (*var)(mask) = tval;
+}
+
+// Blend
+
+template <typename T>
+T Blend(const bool& mask, const T& tval, const T& fval)
+{
+  return mask ? tval : fval;
+}
+
+template <class T, class Mask>
+T Blend(const Mask& mask, const T& tval, const T& fval)
+{
+  T tmp = fval;
+  tmp(mask) = tval;
+  return tmp;
+}
+
+} // namespace vecCore
 #endif
