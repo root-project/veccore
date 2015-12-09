6268f47309fa6d2b2325758e8e58351f52f853ab
Guilherme Amadio <amadio@ncc.unesp.br>
2015-12-09 10:57:56 +0100

    Fix coding style of Vc backends

--- include/VecCore/Backend/VcVector.h
+++ include/VecCore/Backend/VcVector.h
@@ -12,35 +12,35 @@ namespace vecCore {
 namespace backend {
 
 template <typename FloatT = float, bool EReturns = true>
-struct VcVector {
+class VcVector {
   static constexpr bool EarlyReturns = EReturns;
 
-  typedef bool     Bool_t;
-  typedef FloatT   Real_t;
-  typedef float    Float_t;
-  typedef double   Double_t;
+  typedef bool Bool_t;
+  typedef FloatT Real_t;
+  typedef float Float_t;
+  typedef double Double_t;
 
-  typedef typename IntFor<Real_t>::type  Int_t;
+  typedef typename IntFor<Real_t>::type Int_t;
   typedef typename UIntFor<Real_t>::type UInt_t;
 
-  typedef int16_t  Int16_t;
-  typedef int32_t  Int32_t;
-  typedef int64_t  Int64_t;
+  typedef int16_t Int16_t;
+  typedef int32_t Int32_t;
+  typedef int64_t Int64_t;
 
   typedef uint16_t UInt16_t;
   typedef uint32_t UInt32_t;
   typedef uint64_t UInt64_t;
 
-  typedef Vc::Vector<Real_t>   Real_v;
-  typedef Vc::Vector<Float_t>  Float_v;
+  typedef Vc::Vector<Real_t> Real_v;
+  typedef Vc::Vector<Float_t> Float_v;
   typedef Vc::Vector<Double_t> Double_v;
 
-  typedef Vc::Vector<Int_t>    Int_v;
-  typedef Vc::Vector<Int16_t>  Int16_v;
-  typedef Vc::Vector<Int32_t>  Int32_v;
-  typedef Vc::Vector<Int64_t>  Int64_v;
+  typedef Vc::Vector<Int_t> Int_v;
+  typedef Vc::Vector<Int16_t> Int16_v;
+  typedef Vc::Vector<Int32_t> Int32_v;
+  typedef Vc::Vector<Int64_t> Int64_v;
 
-  typedef Vc::Vector<UInt_t>   UInt_v;
+  typedef Vc::Vector<UInt_t> UInt_v;
   typedef Vc::Vector<UInt16_t> UInt16_v;
   typedef Vc::Vector<UInt32_t> UInt32_v;
   typedef Vc::Vector<UInt64_t> UInt64_v;
