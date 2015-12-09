fdc7a6b2443055185b55fca77c1c7a56beb5fc69
Guilherme Amadio <amadio@ncc.unesp.br>
2015-12-09 11:13:03 +0100

    Fix coding style of Timer.h

--- include/VecCore/Timer.h
+++ include/VecCore/Timer.h
@@ -7,22 +7,22 @@ using namespace std::chrono;
 
 template <class unit = nanoseconds>
 class Timer {
-	using clock = high_resolution_clock;
+  using clock = high_resolution_clock;
 public:
-	Timer() : fStart(), fStop() { Start(); }
+  Timer() : fStart(), fStop() { Start(); }
 
-	void Reset() { Start(); }
+  void Reset() { Start(); }
 
-	void Start() { fStart = clock::now(); }
+  void Start() { fStart = clock::now(); }
 
-	double Elapsed()
-	{
-		fStop = clock::now();
-		return duration_cast<unit>(fStop - fStart).count();
-	}
+  double Elapsed()
+  {
+    fStop = clock::now();
+    return duration_cast<unit>(fStop - fStart).count();
+  }
 
 private:
-	high_resolution_clock::time_point fStart, fStop;
+  high_resolution_clock::time_point fStart, fStop;
 };
 
 #if !defined(VECCORE_CUDA_ARCH)
@@ -31,27 +31,27 @@ class cycles {};
 
 template <> class Timer<cycles> {
 public:
-	Timer() { Start(); }
+  Timer() { Start(); }
 
-	void Reset() { Start(); }
+  void Reset() { Start(); }
 
-	void Start() { fStart = GetCycleCount(); }
+  void Start() { fStart = GetCycleCount(); }
 
-	double Elapsed()
-	{
-		return static_cast<double>(GetCycleCount() - fStart);
-	}
+  double Elapsed()
+  {
+    return static_cast<double>(GetCycleCount() - fStart);
+  }
 
 private:
-	unsigned long long int fStart;
+  unsigned long long int fStart;
 
-	inline __attribute__((always_inline))
-	unsigned long long int GetCycleCount()
-	{
-		unsigned int hi, lo;
-		asm volatile("cpuid\n\t" "rdtsc" : "=a"(lo), "=d"(hi));
-		return ((unsigned long long)lo)|(((unsigned long long)hi) << 32);
-	}
+  inline __attribute__((always_inline))
+  unsigned long long int GetCycleCount()
+  {
+    unsigned int hi, lo;
+    asm volatile("cpuid\n\t" "rdtsc" : "=a"(lo), "=d"(hi));
+    return ((unsigned long long)lo)|(((unsigned long long)hi) << 32);
+  }
 };
 #endif
 
