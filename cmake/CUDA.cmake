046e51696cac73051486324277156dfe833bda30
Guilherme Amadio <amadio@ncc.unesp.br>
2016-02-16 17:21:15 -0200

    Add GCC compiler and version check for CUDA

--- cmake/CUDA.cmake
+++ cmake/CUDA.cmake
@@ -1,3 +1,12 @@
+if(NOT "${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
+  message(FATAL_ERROR "CUDA compilation requires GCC compiler")
+else()
+  if (CMAKE_CXX_COMPILER_VERSION VERSION_GREATER 5.0.0
+      OR CMAKE_CXX_COMPILER_VERSION VERSION_EQUAL 5.0.0)
+    message(FATAL_ERROR "GCC ${CMAKE_CXX_COMPILER_VERSION} is not supported by CUDA")
+  endif()
+endif()
+
 find_package(CUDA REQUIRED)
 
 set(VECCORE_ENABLE_CUDA True)
