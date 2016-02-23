25ef722e9aa23361b7dbbda408c41fec204c157d
Guilherme Amadio <amadio@ncc.unesp.br>
2016-02-23 11:06:24 -0300

    [VECGEOM-230] Update backend conformance test using Google Test
    Closes VECGEOM-230.

--- test/backend.cc
+++ test/backend.cc
@@ -1,101 +1,251 @@
-#undef NDEBUG
-#include <cassert>
-#include <cstdio>
-
 #include <VecCore/VecCore>
 
-using namespace vecCore;
+#include <type_traits>
+#include <gtest/gtest.h>
 
-template <class T>
-void TestMask() {
-  using Mask = typename T::Mask;
+using namespace testing;
 
-  Mask mtrue(true), mfalse(false);
-
-  assert(MaskFull(mtrue));
-  assert(MaskEmpty(mfalse));
-
-  assert(MaskFull(mtrue || mfalse));
-  assert(MaskEmpty(mtrue && mfalse));
-}
-
-template <class T>
-void TestReal() {
-  using Mask = typename T::Mask;
-
-  TestMask<T>();
-
-  printf("T::size() = %d\n\n", (int)T::size());
-
-  T x(0.0), y(1.0), z(2.0);
-
-  x = y + z;
-  x = y - z;
-  x = y * z;
-  x = y / z;
-
-  for (size_t i = 0; i < T::size(); i++)
-    x[i] = i + 1;
-
-  Mask mask = (x > T(T::size()) / T(2));
-
-  for (size_t i = 0; i < T::size(); i++)
-    printf("x[%lu] = %g, x[%lu] > %lu == %s\n", i, (double)x[i], i,
-           (size_t)T::size() / 2, mask[i] ? "true" : "false");
-
-  printf("\n");
-}
-
-template <class T>
-void TestInteger() {
-  using Mask = typename T::Mask;
-
-  TestMask<T>();
-
-  printf("T::size() = %d\n\n", (int)T::size());
-
-  T x(0), y(1), z(2);
-
-  x = y + z;
-  x = y - z;
-  x = y * z;
-  x = y / z;
-
-  for (size_t i = 0; i < T::size(); i++)
-    x[i] = i + 1;
-
-  Mask mask = (x > T(T::size()) / T(2));
-
-  for (size_t i = 0; i < T::size(); i++)
-    printf("x[%lu] = %d, x[%lu] > %d == %s\n", i, (int)x[i], i,
-           (int)T::size() / 2, mask[i] ? "true" : "false");
-
-  printf("\n");
-}
+#if defined(GTEST_HAS_TYPED_TEST) && defined(GTEST_HAS_TYPED_TEST_P)
 
 template <class Backend>
-void TestBackend() {
-  TestReal<typename Backend::Float_v>();
-  TestReal<typename Backend::Double_v>();
-  TestInteger<typename Backend::Int32_v>();
-  TestInteger<typename Backend::UInt32_v>();
+using FloatTypes = Types
+<
+  typename Backend::Float_v,
+  typename Backend::Double_v
+>;
+
+template <class Backend>
+using IntTypes = Types
+<
+  typename Backend::Int16_v,
+  typename Backend::UInt16_v,
+  typename Backend::Int32_v,
+  typename Backend::UInt32_v
+>;
+
+template <class Backend>
+using VectorTypes = Types
+<
+  typename Backend::Float_v,
+  typename Backend::Double_v,
+  typename Backend::Int16_v,
+  typename Backend::UInt16_v,
+  typename Backend::Int32_v,
+  typename Backend::UInt32_v
+>;
+
+///////////////////////////////////////////////////////////////////////////////
+
+template <class T> class VectorTypeTest : public Test {
+public:
+  using Scalar_t = typename vecCore::ScalarType<T>::Type;
+  using Vector_t = T;
+};
+
+///////////////////////////////////////////////////////////////////////////////
+
+template <class T> class ConstructorTest : public VectorTypeTest<T> {};
+
+TYPED_TEST_CASE_P(ConstructorTest);
+
+TYPED_TEST_P(ConstructorTest, Default)
+{
+  using Vector_t = typename TestFixture::Vector_t;
+
+  EXPECT_TRUE((std::is_constructible<Vector_t>::value));
 }
 
-int main(int argc, char *argv[]) {
+TYPED_TEST_P(ConstructorTest, Copy)
+{
+  using Vector_t = typename TestFixture::Vector_t;
 
-  printf("Backend Test: %s\n\n", "ScalarWrapper");
-  TestBackend<backend::ScalarWrapper>();
+  EXPECT_TRUE((std::is_copy_constructible<Vector_t>::value));
+}
+
+TYPED_TEST_P(ConstructorTest, Move)
+{
+  using Vector_t = typename TestFixture::Vector_t;
+
+  EXPECT_TRUE((std::is_move_constructible<Vector_t>::value));
+}
+
+TYPED_TEST_P(ConstructorTest, FromScalar)
+{
+  using Scalar_t = typename TestFixture::Scalar_t;
+  using Vector_t = typename TestFixture::Vector_t;
+
+  EXPECT_TRUE((std::is_constructible<Vector_t, Scalar_t>::value));
+}
+
+TYPED_TEST_P(ConstructorTest, FromRefToScalar)
+{
+  using Scalar_t = typename TestFixture::Scalar_t;
+  using Vector_t = typename TestFixture::Vector_t;
+
+  EXPECT_TRUE((std::is_constructible<Vector_t, const Scalar_t &>::value));
+}
+
+REGISTER_TYPED_TEST_CASE_P(ConstructorTest, Default, Copy, Move, FromScalar, FromRefToScalar);
+
+///////////////////////////////////////////////////////////////////////////////
+
+template <class T> class ArithmeticsTest : public VectorTypeTest<T> {};
+
+TYPED_TEST_CASE_P(ArithmeticsTest);
+
+TYPED_TEST_P(ArithmeticsTest, Addition)
+{
+  using Scalar_t = typename TestFixture::Scalar_t;
+  using Vector_t = typename TestFixture::Vector_t;
+
+  Vector_t lhs(static_cast<Scalar_t>(1));
+  Vector_t rhs(static_cast<Scalar_t>(1));
+  Vector_t res(static_cast<Scalar_t>(2));
+
+  EXPECT_TRUE(vecCore::MaskFull(res == (lhs + rhs)));
+}
+
+TYPED_TEST_P(ArithmeticsTest, Subtraction)
+{
+  using Scalar_t = typename TestFixture::Scalar_t;
+  using Vector_t = typename TestFixture::Vector_t;
+
+  Vector_t lhs(static_cast<Scalar_t>(3));
+  Vector_t rhs(static_cast<Scalar_t>(1));
+  Vector_t res(static_cast<Scalar_t>(2));
+
+  EXPECT_TRUE(vecCore::MaskFull(res == (lhs - rhs)));
+}
+
+TYPED_TEST_P(ArithmeticsTest, Multiplication)
+{
+  using Scalar_t = typename TestFixture::Scalar_t;
+  using Vector_t = typename TestFixture::Vector_t;
+
+  Vector_t lhs(static_cast<Scalar_t>(2));
+  Vector_t rhs(static_cast<Scalar_t>(2));
+  Vector_t res(static_cast<Scalar_t>(4));
+
+  EXPECT_TRUE(vecCore::MaskFull(res == (lhs * rhs)));
+}
+
+TYPED_TEST_P(ArithmeticsTest, Division)
+{
+  using Scalar_t = typename TestFixture::Scalar_t;
+  using Vector_t = typename TestFixture::Vector_t;
+
+  Vector_t lhs(static_cast<Scalar_t>(10));
+  Vector_t rhs(static_cast<Scalar_t>(5));
+  Vector_t res(static_cast<Scalar_t>(2));
+
+  EXPECT_TRUE(vecCore::MaskFull(res == (lhs / rhs)));
+}
+
+REGISTER_TYPED_TEST_CASE_P(ArithmeticsTest, Addition, Subtraction, Multiplication, Division);
+
+///////////////////////////////////////////////////////////////////////////////
+
+template <class T> class VectorInterfaceTest : public VectorTypeTest<T> {};
+
+TYPED_TEST_CASE_P(VectorInterfaceTest);
+
+TYPED_TEST_P(VectorInterfaceTest, VectorSize)
+{
+  using Vector_t = typename TestFixture::Vector_t;
+
+  EXPECT_TRUE(vecCore::VectorSize<Vector_t>() > 0);
+}
+
+TYPED_TEST_P(VectorInterfaceTest, VectorSizeVariable)
+{
+  using Vector_t = typename TestFixture::Vector_t;
+
+  Vector_t x;
+
+  EXPECT_TRUE(vecCore::VectorSize(x) > 0);
+}
+
+REGISTER_TYPED_TEST_CASE_P(VectorInterfaceTest, VectorSize, VectorSizeVariable);
+
+///////////////////////////////////////////////////////////////////////////////
+
+template <class T> class VectorMaskTest : public VectorTypeTest<T> {};
+
+TYPED_TEST_CASE_P(VectorMaskTest);
+
+TYPED_TEST_P(VectorMaskTest, Constructor)
+{
+  using Vector_t = typename TestFixture::Vector_t;
+
+  EXPECT_TRUE(std::is_constructible<vecCore::Mask_v<Vector_t>>::value);
+  EXPECT_TRUE(std::is_copy_constructible<vecCore::Mask_v<Vector_t>>::value);
+  EXPECT_TRUE(std::is_move_constructible<vecCore::Mask_v<Vector_t>>::value);
+}
+
+TYPED_TEST_P(VectorMaskTest, MaskFull)
+{
+  using Vector_t = typename TestFixture::Vector_t;
+
+  vecCore::Mask_v<Vector_t> tmask(true), fmask(false);
+
+  EXPECT_TRUE(vecCore::MaskFull(tmask));
+  EXPECT_TRUE(vecCore::MaskFull(!fmask));
+}
+
+TYPED_TEST_P(VectorMaskTest, MaskEmpty)
+{
+  using Vector_t = typename TestFixture::Vector_t;
+
+  vecCore::Mask_v<Vector_t> tmask(true), fmask(false);
+
+  EXPECT_TRUE(vecCore::MaskEmpty(fmask));
+  EXPECT_TRUE(vecCore::MaskEmpty(!tmask));
+}
+
+TYPED_TEST_P(VectorMaskTest, MaskAssign)
+{
+  using Vector_t = typename TestFixture::Vector_t;
+
+  Vector_t a(0), b(1);
+
+  vecCore::MaskedAssign(a, a > b, b);
+}
+
+TYPED_TEST_P(VectorMaskTest, Blend)
+{
+  using Vector_t = typename TestFixture::Vector_t;
+
+  Vector_t a(0), b(1);
+
+  a = vecCore::Blend(a > b, a, b);
+}
+
+REGISTER_TYPED_TEST_CASE_P(VectorMaskTest, Constructor, MaskFull, MaskEmpty, MaskAssign, Blend);
+
+#define TEST_BACKEND_P(name, x)                                                            \
+  INSTANTIATE_TYPED_TEST_CASE_P(name, ConstructorTest, VectorTypes<vecCore::backend::x>);  \
+  INSTANTIATE_TYPED_TEST_CASE_P(name, ArithmeticsTest, VectorTypes<vecCore::backend::x>);  \
+  INSTANTIATE_TYPED_TEST_CASE_P(name, VectorMaskTest, VectorTypes<vecCore::backend::x>);   \
+  INSTANTIATE_TYPED_TEST_CASE_P(name, VectorInterfaceTest, VectorTypes<vecCore::backend::x>)
+
+#define TEST_BACKEND(x) TEST_BACKEND_P(x, x)
+
+///////////////////////////////////////////////////////////////////////////////
+
+TEST_BACKEND(Scalar);
+TEST_BACKEND(ScalarWrapper);
 
 #ifdef VECCORE_ENABLE_VC
-  printf("Backend Test: %s\n\n", "Vc (Scalar)");
-  TestBackend<backend::VcScalar>();
-
-  printf("Backend Test: %s\n\n", "Vc (Vector)");
-  TestBackend<backend::VcVector>();
-
-  printf("Backend Test: %s\n\n", "Vc (SimdArray<16>)");
-  TestBackend<backend::VcSimdArray<16> >();
+TEST_BACKEND(VcScalar);
+TEST_BACKEND(VcVector);
+TEST_BACKEND_P(VcSimdArray, VcSimdArray<16>);
 #endif
 
-  return 0;
+#else // if !GTEST_HAS_TYPED_TEST
+TEST(DummyTest, TypedTestsAreNotSupportedOnThisPlatform) {}
+#endif
+
+int main(int argc, char *argv[]) {
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
