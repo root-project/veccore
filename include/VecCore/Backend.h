c37a1cdfac5d90186595e717bf65c97001af85fd
Guilherme Amadio <amadio@ncc.unesp.br>
2016-02-24 19:04:57 -0300

    Provide scalar backend implementation in Backend.h

--- include/VecCore/Backend.h
+++ include/VecCore/Backend.h
@@ -5,9 +5,9 @@
 #include "Limits.h"
 #include "Types.h"
 
-// backend interface
-
 namespace vecCore {
+
+// backend interface
 
 template <class Mask> Bool_t IsEmpty(const Mask mask);
 template <class Mask> Bool_t IsFull(const Mask mask);
@@ -17,6 +17,38 @@ void MaskAssign(T& dest, bool mask, const T &src);
 
 template <class T, class Mask>
 T Blend(const Mask mask, const T& tval, const T& fval);
+
+// scalar backend implementation
+
+template <>
+VECCORE_FORCE_INLINE
+Bool_t IsEmpty<Bool_t>(const Bool_t mask)
+{
+  return !mask;
+}
+
+template <>
+VECCORE_FORCE_INLINE
+Bool_t IsFull<Bool_t>(const Bool_t mask)
+{
+  return mask;
+}
+
+template <typename T>
+VECCORE_FORCE_INLINE
+void MaskAssign(T& dest, Bool_t mask, const T &src)
+{
+  if (mask) dest = src;
+}
+
+template <typename T>
+VECCORE_FORCE_INLINE
+T Blend(const Bool_t mask, const T& tval, const T& fval)
+{
+  return mask ? tval : fval;
+}
+
+// extra backend functions
 
 VECCORE_FORCE_INLINE
 VECCORE_CUDA_HOST_DEVICE
--- include/VecCore/Backend/Basic.h
+++ include/VecCore/Backend/Basic.h
@@ -9,34 +9,6 @@
 
 namespace vecCore {
 
-template <>
-VECCORE_FORCE_INLINE
-Bool_t IsEmpty<Bool_t>(const Bool_t mask)
-{
-  return !mask;
-}
-
-template <>
-VECCORE_FORCE_INLINE
-Bool_t IsFull<Bool_t>(const Bool_t mask)
-{
-  return mask;
-}
-
-template <typename T, typename = std::enable_if<std::is_scalar<T>::value>>
-VECCORE_FORCE_INLINE
-void MaskAssign(T& dest, Bool_t mask, const T &src)
-{
-  if (mask) dest = src;
-}
-
-template <typename T, typename = std::enable_if<std::is_scalar<T>::value>>
-VECCORE_FORCE_INLINE
-T Blend(const Bool_t mask, const T& tval, const T& fval)
-{
-  return mask ? tval : fval;
-}
-
 namespace backend {
 
 class Basic {
