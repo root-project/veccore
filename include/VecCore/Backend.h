3d55acbe5daf2fc8f7b2bb19ba9aa31cc9ad313e
Guilherme Amadio <amadio@ncc.unesp.br>
2016-03-10 10:02:28 -0600

    Remove CUDA backend
    CUDA is not really a backend, but a platform. CUDA should use the
    Scalar backend directly.

--- include/VecCore/Backend.h
+++ include/VecCore/Backend.h
@@ -30,7 +30,6 @@ constexpr Bool_t EarlyReturnAllowed()
 
 #include "Backend/Basic.h"
 #include "Backend/Scalar.h"
-#include "Backend/CUDA.h"
 
 #if !defined(VECCORE_NVCC)
 #include "Backend/VcScalar.h"
