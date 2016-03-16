b16f63c0d9594399870df3b770267a048ae8e542
Guilherme Amadio <amadio@ncc.unesp.br>
2016-03-16 17:25:49 -0500

    Fix problem with multiple flags in target ISA options

--- cmake/Clang.cmake
+++ cmake/Clang.cmake
@@ -5,7 +5,7 @@ set(FLAGS_SSSE3 "-mssse3")
 set(FLAGS_SSE41 "-msse4.1")
 set(FLAGS_SSE42 "-msse4.2")
 set(FLAGS_AVX   "-mavx")
-set(FLAGS_AVX2  "-mf16c -mavx2 -mfma -mlzcnt -mbmi -mbmi2")
+set(FLAGS_AVX2  "-mf16c;-mavx2;-mfma;-mlzcnt;-mbmi;-mbmi2")
 set(FLAGS_AVX512 "-mavx512")
 set(FLAGS_NATIVE "-march=native")
 
