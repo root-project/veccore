10559fef67302408f01fe317056c140477e9bd1c
Guilherme Amadio <amadio@ncc.unesp.br>
2016-05-18 13:53:28 -0300

    [VECCORE-4] Improve quadratic example to test more backends

--- examples/quadratic.cc
+++ examples/quadratic.cc
@@ -10,22 +10,39 @@
 
 using namespace vecCore;
 
-#ifdef VECCORE_DISABLE_SIMD
-using Backend = backend::Scalar;
-#elif defined(VECCORE_ENABLE_VC)
-using Backend = backend::VcVector;
-#elif defined(VECCORE_ENABLE_UMESIMD)
-using Backend = backend::UMESimd;
-#else
-using Backend = backend::ScalarWrapper;
-#endif
+static const int N = (8 * 1024 * 1024);
 
-static const Int_s N = (8 * 1024 * 1024);
+// solve ax2 + bx + c = 0
 
-// solve ax2 + bx + c = 0, return number of roots found
+// naive scalar code
 
 template <typename T>
-void QuadSolve(const T& a, const T& b, const T& c, T &x1, T &x2, int& roots) {
+int QuadSolveNaive(T a, T b, T c, T &x1, T &x2) {
+  T delta = b * b - 4.0 * a * c;
+
+  if (delta < 0.0)
+    return 0;
+
+  if (delta < NumericLimits<T>::Epsilon()) {
+    x1 = x2 = -0.5 * b / a;
+    return 1;
+  }
+
+  if (b >= 0.0) {
+    x1 = -0.5 * (b + math::Sqrt(delta)) / a;
+    x2 = c / (a * x1);
+  } else {
+    x2 = -0.5 * (b - math::Sqrt(delta)) / a;
+    x1 = c / (a * x2);
+  }
+
+  return 2;
+}
+
+// optimized scalar code
+
+template <typename T>
+void QuadSolveOptimized(const T& a, const T& b, const T& c, T &x1, T &x2, int& roots) {
   T a_inv = T(1.0) / a;
   T delta = b * b - T(4.0) * a * c;
   T s = (b >= 0) ? T(1.0) : T(-1.0);
@@ -34,7 +51,7 @@ void QuadSolve(const T& a, const T& b, const T& c, T &x1, T &x2, int& roots) {
 
   switch (roots) {
     case 2:
-      x1 = T(0.5) * (-b + s * math::Sqrt(delta));
+      x1 = T(-0.5) * (b + s * math::Sqrt(delta));
       x2 = c / x1;
       x1 *= a_inv;
       return;
@@ -49,10 +66,7 @@ void QuadSolve(const T& a, const T& b, const T& c, T &x1, T &x2, int& roots) {
   }
 }
 
-// SIMD version
-
-// solve ax2 + bx + c = 0, return number of roots found
-// solves many equation simultaneously, depending on value of Float_v::Size
+// explicit SIMD code
 
 template <class Backend>
 void QuadSolveSIMD(typename Backend::Float_v const &a,
@@ -67,53 +81,67 @@ void QuadSolveSIMD(typename Backend::Float_v const &a,
   using FMask = Mask_v<Float_v>;
   using IMask = Mask_v<Int32_v>;
 
-  roots = 0;
-  Float_v delta = b * b - Float_v(4.0) * a * c;
+  Float_v a_inv = Float_v(1.0f) / a;
+  Float_v delta = b * b - Float_v(4.0f) * a * c;
+  Float_v sign = Blend(FMask(b >= Float_v(0.0f)), Float_v(1.0f), Float_v(-1.0f));
 
-  FMask mask0(delta < Float_v(0.0));
+  FMask mask0(delta < Float_v(0.0f));
   FMask mask2(delta >= NumericLimits<Float_v>::Epsilon());
 
-  MaskedAssign(roots, (IMask)mask2, Int32_v(2));
+  Float_v root1 = Float_v(-0.5f) * (b + sign * math::Sqrt(delta));
+  Float_v root2 = c / root1;
+  root1 = root1 * a_inv;
 
-  FMask mask = (b >= Float_v(0.0));
+  FMask mask1 = !(mask2 || mask0);
 
-  Float_v sign = Blend(mask, Float_v(-1.0), Float_v(1.0));
-  Float_v root = (-b + sign * math::Sqrt(delta)) / (Float_v(2.0) * a);
-
-  MaskedAssign(x1, mask2 && mask,  root);
-  MaskedAssign(x2, mask2 && !mask, root);
-
-  root = c / (a * Blend(mask, x1, x2));
-
-  MaskedAssign(x1, mask2 && !mask, root);
-  MaskedAssign(x2, mask2 && mask,  root);
-
-  FMask mask1 = !(mask0 || mask2);
+  MaskedAssign(x1, mask2, root1);
+  MaskedAssign(x2, mask2, root2);
+  roots = Blend((IMask)mask2, Int32_v(2), Int32_v(0));
 
   if (MaskEmpty(mask1))
     return;
 
+  root1 = Float_v(-0.5f) * b * a_inv;
   MaskedAssign(roots, (IMask)mask1, Int32_v(1));
+  MaskedAssign(x1, mask1, root1);
+  MaskedAssign(x2, mask1, root1);
+}
 
-  root = -Float_v(0.5) * b / a;
+template <class Backend>
+void TestQuadSolve(const float* a, const float* b, const float* c, float* x1, float* x2, int* roots,
+                   size_t N, const char* name)
+{
+  using Float_v = typename Backend::Float_v;
+  using Int32_v = typename Backend::Int32_v;
 
-  MaskedAssign(x1, mask1, root);
-  MaskedAssign(x2, mask1, root);
+  Timer<milliseconds> timer;
+  for (size_t i = 0; i < N; i += VectorSize<Float_v>())
+    QuadSolveSIMD<Backend>(
+      (Float_v &)(a[i]),  (Float_v &)(b[i]),  (Float_v &)(c[i]),
+      (Float_v &)(x1[i]), (Float_v &)(x2[i]), (Int32_v &)(roots[i]));
+  double t = timer.Elapsed();
+
+#ifdef VERBOSE
+  int index = (int)((N - 100) * drand48());
+  for (int i = index; i < index + 10; i++)
+    printf("%d: a = % 8.3f, b = % 8.3f, c = % 8.3f, roots = %d, x1 = % 8.3f, x2 = % 8.3f\n",
+            i, a[i], b[i], c[i], roots[i], roots[i] > 0 ? x1[i] : 0, roots[i] > 1 ? x2[i] : 0);
+#endif
+  printf("\nelapsed time = %.3lfms (%s)\n", t, name);
 }
 
 int main(int argc, char *argv[]) {
-  Float_s *a = (Float_s *)memalign(64, N * sizeof(Float_s));
-  Float_s *b = (Float_s *)memalign(64, N * sizeof(Float_s));
-  Float_s *c = (Float_s *)memalign(64, N * sizeof(Float_s));
+  float *a = (float *)memalign(64, N * sizeof(float));
+  float *b = (float *)memalign(64, N * sizeof(float));
+  float *c = (float *)memalign(64, N * sizeof(float));
 
-  Int_s *roots = (Int_s *)memalign(64, N * sizeof(Int_s));
-  Float_s *x1 = (Float_s *)memalign(64, N * sizeof(Float_s));
-  Float_s *x2 = (Float_s *)memalign(64, N * sizeof(Float_s));
+  int *roots = (int *)memalign(64, N * sizeof(int));
+  float *x1 = (float *)memalign(64, N * sizeof(float));
+  float *x2 = (float *)memalign(64, N * sizeof(float));
 
   srand48(time(NULL));
-  Int_s index = (Int_s)((N - 100) * drand48());
 
-  for (Int_s i = 0; i < N; i++) {
+  for (int i = 0; i < N; i++) {
     a[i] = 10.0 * (drand48() - 0.5);
     b[i] = 10.0 * (drand48() - 0.5);
     c[i] = 50.0 * (drand48() - 0.5);
@@ -123,49 +151,45 @@ int main(int argc, char *argv[]) {
   }
 
   Timer<milliseconds> timer;
+  for (int i = 0; i < N; i++)
+    roots[i] = QuadSolveNaive(a[i], b[i], c[i], x1[i], x2[i]);
+  double t = timer.Elapsed();
 
-  for (Int_s i = 0; i < N; i++) {
-    QuadSolve<float>(a[i], b[i], c[i], x1[i], x2[i], roots[i]);
-  }
-
-  Double_s t = timer.Elapsed();
-
-  // print random result to ensure scalar and vector backends give same result
-
-  for (Int_s i = index; i < index + 10; i++) {
-    printf("%d: a = % 8.3f, b = % 8.3f, c = % 8.3f,"
-           " roots = %d, x1 = % 8.3f, x2 = % 8.3f\n",
-            i, a[i], b[i], c[i], roots[i],
-            roots[i] > 0 ? x1[i] : 0,
-            roots[i] > 1 ? x2[i] : 0);
-  }
-
-  printf("\nelapsed time = %.3lfms (scalar code)\n", t);
+#ifdef VERBOSE
+  int index = (int)((N - 100) * drand48());
+  // print random values to check results across backends
+  for (int i = index; i < index + 10; i++)
+    printf("%d: a = % 8.3f, b = % 8.3f, c = % 8.3f, roots = %d, x1 = % 8.3f, x2 = % 8.3f\n",
+            i, a[i], b[i], c[i], roots[i], roots[i] > 0 ? x1[i] : 0, roots[i] > 1 ? x2[i] : 0);
+#endif
+  printf("\nelapsed time = %.3lfms (naive scalar code)\n", t);
 
   timer.Start();
-
-  for (Int_s i = 0; i < N; i += Int_s(VectorSize<Backend::Float_v>())) {
-    QuadSolveSIMD<Backend>(
-      (Backend::Float_v &)(a[i]),
-      (Backend::Float_v &)(b[i]),
-      (Backend::Float_v &)(c[i]),
-      (Backend::Float_v &)(x1[i]),
-      (Backend::Float_v &)(x2[i]),
-      (Backend::Int32_v &)(roots[i])
-    );
-  }
-
+  for (int i = 0; i < N; i++)
+    QuadSolveOptimized<float>(a[i], b[i], c[i], x1[i], x2[i], roots[i]);
   t = timer.Elapsed();
 
-  for (Int_s i = index; i < index + 10; i++) {
-    printf("%d: a = % 8.3f, b = % 8.3f, c = % 8.3f,"
-           " roots = %d, x1 = % 8.3f, x2 = % 8.3f\n",
-            i, a[i], b[i], c[i], roots[i],
-            roots[i] > 0 ? x1[i] : 0,
-            roots[i] > 1 ? x2[i] : 0);
-  }
+#ifdef VERBOSE
+  for (int i = index; i < index + 10; i++)
+    printf("%d: a = % 8.3f, b = % 8.3f, c = % 8.3f, roots = %d, x1 = % 8.3f, x2 = % 8.3f\n",
+            i, a[i], b[i], c[i], roots[i], roots[i] > 0 ? x1[i] : 0, roots[i] > 1 ? x2[i] : 0);
+#endif
+  printf("\nelapsed time = %.3lfms (optimized scalar code)\n", t);
 
-  printf("\nelapsed time = %.3lfms (vector backend)\n", t);
+  TestQuadSolve<backend::Scalar>(a, b, c, x1, x2, roots, N, "plain scalar backend");
+  TestQuadSolve<backend::ScalarWrapper>(a, b, c, x1, x2, roots, N, "scalarwrapper backend");
+
+#ifdef VECCORE_ENABLE_VC
+  TestQuadSolve<backend::VcScalar>(a, b, c, x1, x2, roots, N, "simd code via Vc scalar backend");
+  TestQuadSolve<backend::VcVector>(a, b, c, x1, x2, roots, N, "simd code via Vc vector backend");
+  TestQuadSolve<backend::VcSimdArray<8>>(a, b, c, x1, x2, roots, N, "simd code via VcSimdArray<8> backend");
+  TestQuadSolve<backend::VcSimdArray<16>>(a, b, c, x1, x2, roots, N, "simd code via VcSimdArray<16> backend");
+  TestQuadSolve<backend::VcSimdArray<32>>(a, b, c, x1, x2, roots, N, "simd code via VcSimdArray<32> backend");
+#endif
+
+#ifdef VECCORE_ENABLE_UMESIMD
+  TestQuadSolve<backend::UMESimd>(a, b, c, x1, x2, roots, N, "simd code via UME::SIMD backend");
+#endif
 
   return 0;
 }
