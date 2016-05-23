5307cf653f8cfec49b82380ded9e8510bb75a65a
Sandro Wenzel <sandro.wenzel@cern.ch>
2016-05-23 16:30:23 +0200

    Fix typo in SIMDSizes

--- include/VecCore/Backend/SIMDSizes.h
+++ include/VecCore/Backend/SIMDSizes.h
@@ -11,7 +11,7 @@ template <typename T> constexpr size_t SIMDWidth() { return 1; }
 #ifdef __AVX512__
 template <> constexpr size_t SIMDWidth<Double_s>() { return 8;  }
 template <> constexpr size_t SIMDWidth<Float_s>()  { return 16; }
-template <> constexpr size_t SIMDWidth<UInt_s>()   { return 16; }
+template <> constexpr size_t SIMDWidth<UInt32_s>() { return 16; }
 template <> constexpr size_t SIMDWidth<Int32_s>()  { return 16; }
 template <> constexpr size_t SIMDWidth<UInt16_s>() { return 32; }
 template <> constexpr size_t SIMDWidth<Int16_s>()  { return 32; }
