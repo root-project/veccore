0d842f37fc9d0a74be6421460658ac852b32ba11
Guilherme Amadio <amadio@ncc.unesp.br>
2016-06-09 17:26:54 -0300

    [VECCORE-13] Update VecCore CMake build system
    - Add CUDA hello world test to make sure CUDA setup is OK
    - Update bench/CMakeLists.txt to run benchmarks by defult
    - Remove Test- prefix from test executables, as they are already
      placed inside a test directory anyway
    - Rework main CMakeLists.txt to fit with new setup and simplify
      build options

--- cmake/CompilerSetup.cmake
+++ cmake/CompilerSetup.cmake
@@ -1,24 +1,28 @@
+set(TARGET_ISA "native" CACHE STRING "Select target instruction set architecture")
 
-set(TARGET_ISA "native" CACHE STRING "Selects target instruction set architecture to support.")
-set_property(CACHE TARGET_ISA PROPERTY STRINGS native sse sse2 sse3 ssse3 sse41 sse42 avx avx2 avx512 mic)
-
-message(STATUS "Compiling for ${TARGET_ISA} SIMD instructions")
+string(TOUPPER "${TARGET_ISA}" TARGET_ISA)
+if (NOT "${TARGET_ISA}" MATCHES "^(NATIVE|SSE(2|3|4.(1|2))|AVX(|2|512)|KNC|KNL)$")
+  message(FATAL_ERROR "Unknown target instruction set architecture: ${TARGET_ISA}")
+endif()
 
 add_compile_options(-Wall)
 
-if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
-  include (GCC)
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" OR
-        "${CMAKE_CXX_COMPILER_ID}" STREQUAL "AppleClang")
-  include (Clang)
-elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel")
-  include (Intel)
+if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "(Apple|)Clang|GNU|Intel")
+  include(${CMAKE_CXX_COMPILER_ID})
 else()
   message(WARNING "Unsupported compiler: ${CMAKE_CXX_COMPILER_ID}")
 endif()
 
-string(TOUPPER "FLAGS_${TARGET_ISA}" _target_isa)
-set(CMAKE_CXX_FLAGS_TARGET_ISA "${${_target_isa}}")
+string(REPLACE "." "" ISA "${TARGET_ISA}")
+
+if(NOT DEFINED FLAGS_${ISA})
+  set(COMPILER "${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}")
+  message(FATAL_ERROR "${TARGET_ISA} not supported with ${COMPILER} compiler")
+endif()
+
+message(STATUS "Compiling for ${TARGET_ISA} SIMD instructions")
+
+add_compile_options(${FLAGS_${ISA}})
 
 if (CUDA)
   include(CUDA)
