10b40aa72b901fe535dc7ae22e33b97bbc546b8b
Guilherme Amadio <amadio@ncc.unesp.br>
2016-08-23 15:02:54 -0300

    Update VecCore's CompilerSetup.cmake module
    This commit leaves the default for TARGET_ISA unset, as that is the
    usual expected behavior for such an option. The user must choose the
    desired instruction set explicitly by passing e.g. -DTARGET_ISA=avx2.
    This configuration flag now supports building for Knights Corner with
    -DTARGET_ISA=KNC and for Knights Landing with -DTARGET_ISA=KNL, as well.

--- cmake/CompilerSetup.cmake
+++ cmake/CompilerSetup.cmake
@@ -1,10 +1,3 @@
-set(TARGET_ISA "native" CACHE STRING "Select target instruction set architecture")
-
-string(TOUPPER "${TARGET_ISA}" TARGET_ISA)
-if (NOT "${TARGET_ISA}" MATCHES "^(NATIVE|SSE(2|3|4.(1|2))|AVX(|2|512)|KNC|KNL)$")
-  message(FATAL_ERROR "Unknown target instruction set architecture: ${TARGET_ISA}")
-endif()
-
 add_compile_options(-Wall)
 
 if ("${CMAKE_CXX_COMPILER_ID}" MATCHES "(Apple|)Clang|GNU|Intel")
@@ -13,13 +6,24 @@ else()
   message(WARNING "Unsupported compiler: ${CMAKE_CXX_COMPILER_ID}")
 endif()
 
-string(REPLACE "." "" ISA "${TARGET_ISA}")
+if (DEFINED TARGET_ISA)
+  string(TOUPPER "${TARGET_ISA}" TARGET_ISA)
 
-if(NOT DEFINED FLAGS_${ISA})
-  set(COMPILER "${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}")
-  message(FATAL_ERROR "${TARGET_ISA} not supported with ${COMPILER} compiler")
+  if (NOT "${TARGET_ISA}" MATCHES "^(NATIVE|SSE(2|3|4.(1|2))|AVX(|2|512)|KNC|KNL)$")
+    message(FATAL_ERROR "Unknown instruction set architecture: ${TARGET_ISA}")
+  endif()
+
+  string(REPLACE "." "" ISA "${TARGET_ISA}")
+
+  if (NOT DEFINED FLAGS_${ISA})
+    set(COMPILER "${CMAKE_CXX_COMPILER_ID} ${CMAKE_CXX_COMPILER_VERSION}")
+    message(FATAL_ERROR "${TARGET_ISA} not supported by ${COMPILER} compiler")
+  else()
+    add_compile_options(${FLAGS_${ISA}})
+  endif()
+
+  if (NOT DEFINED TARGET_ISA_FLAGS_SET)
+    set(TARGET_ISA_FLAGS_SET True CACHE BOOL "")
+    message(STATUS "Compiling for ${TARGET_ISA} instruction set architecture")
+  endif()
 endif()
-
-message(STATUS "Compiling for ${TARGET_ISA} SIMD instructions")
-
-add_compile_options(${FLAGS_${ISA}})
