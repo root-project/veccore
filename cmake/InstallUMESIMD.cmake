599ac85f43927767b371583ade9d360d0ab93a98
Guilherme Amadio <amadio@cern.ch>
2017-03-22 10:12:15 -0300

    Bump version of UME::SIMD to 0.8.1
    This version adds the required version macros to the main header.
    This commit also moves from a git clone to downloading a release
    tarball.

--- cmake/InstallUMESIMD.cmake
+++ cmake/InstallUMESIMD.cmake
@@ -1,21 +1,22 @@
 include(ExternalProject)
 
-set(UMESIMD_VERSION "0.6.1")
+set(UMESIMD_VERSION "0.8.1")
 set(UMESIMD_PROJECT "umesimd-${UMESIMD_VERSION}")
 set(UMESIMD_SRC_URI "https://github.com/edanor/umesimd")
+set(UMESIMD_SRC_MD5 "3960a11a20a4294e68480ab0d39db6d7")
 set(UMESIMD_DESTDIR "${CMAKE_BINARY_DIR}/${UMESIMD_PROJECT}")
 
 ExternalProject_Add(${UMESIMD_PROJECT}
   PREFIX externals
-  GIT_REPOSITORY "${UMESIMD_SRC_URI}"
-  GIT_TAG "v${UMESIMD_VERSION}-stable"
+  URL "${UMESIMD_SRC_URI}/archive/v${UMESIMD_VERSION}.tar.gz"
+  URL_MD5 ${UMESIMD_SRC_MD5}
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND
   install -d ${UMESIMD_DESTDIR}/include/umesimd
   && cd ${UMESIMD_DESTDIR}/include/umesimd
   && cp -nRT ${CMAKE_BINARY_DIR}/externals/src/${UMESIMD_PROJECT} .
-  && rm -rf .git doc examples microbenchmarks unittest README.md
+  && rm -rf CMakeLists.txt README.md doc examples microbenchmarks unittest
 )
 
 add_custom_target(UMESIMD)
