69920eb65a3e50d52b133a3b759c7e379f3c3ab0
Guilherme Amadio <amadio@cern.ch>
2017-04-27 08:49:24 +0200

    Fix UMESIMD_INCLUDE_DIRS in UME::SIMD external

--- cmake/InstallUMESIMD.cmake
+++ cmake/InstallUMESIMD.cmake
@@ -4,7 +4,10 @@ set(UMESIMD_VERSION "0.8.1")
 set(UMESIMD_PROJECT "umesimd-${UMESIMD_VERSION}")
 set(UMESIMD_SRC_URI "https://github.com/edanor/umesimd")
 set(UMESIMD_SRC_MD5 "3960a11a20a4294e68480ab0d39db6d7")
-set(UMESIMD_SRC_DIR "${CMAKE_BINARY_DIR}/externals/src/${UMESIMD_PROJECT}")
+set(UMESIMD_ROOTDIR "${CMAKE_BINARY_DIR}/${UMESIMD_PROJECT}")
+set(UMESIMD_DESTDIR "${CMAKE_BINARY_DIR}/${UMESIMD_PROJECT}/include/umesimd")
+set(UMESIMD_SOURCES "${CMAKE_BINARY_DIR}/externals/src/${UMESIMD_PROJECT}")
+
 set(UMESIMD_DST_DIR "${CMAKE_BINARY_DIR}/${UMESIMD_PROJECT}/include/umesimd")
 
 ExternalProject_Add(${UMESIMD_PROJECT}
@@ -14,22 +17,22 @@ ExternalProject_Add(${UMESIMD_PROJECT}
   CONFIGURE_COMMAND ""
   BUILD_COMMAND ""
   INSTALL_COMMAND
-    ${CMAKE_COMMAND} -E copy_directory ${UMESIMD_SRC_DIR}/ ${UMESIMD_DST_DIR}
-    && ${CMAKE_COMMAND} -E remove ${UMESIMD_DST_DIR}/CMakeLists.txt
-    && ${CMAKE_COMMAND} -E remove ${UMESIMD_DST_DIR}/README.md
-    && ${CMAKE_COMMAND} -E remove_directory ${UMESIMD_DST_DIR}/doc
-    && ${CMAKE_COMMAND} -E remove_directory ${UMESIMD_DST_DIR}/examples
-    && ${CMAKE_COMMAND} -E remove_directory ${UMESIMD_DST_DIR}/microbenchmarks
-    && ${CMAKE_COMMAND} -E remove_directory ${UMESIMD_DST_DIR}/unittest
+    ${CMAKE_COMMAND} -E copy_directory ${UMESIMD_SOURCES}/ ${UMESIMD_DESTDIR}
+    && ${CMAKE_COMMAND} -E remove ${UMESIMD_DESTDIR}/CMakeLists.txt
+    && ${CMAKE_COMMAND} -E remove ${UMESIMD_DESTDIR}/README.md
+    && ${CMAKE_COMMAND} -E remove_directory ${UMESIMD_DESTDIR}/doc
+    && ${CMAKE_COMMAND} -E remove_directory ${UMESIMD_DESTDIR}/examples
+    && ${CMAKE_COMMAND} -E remove_directory ${UMESIMD_DESTDIR}/microbenchmarks
+    && ${CMAKE_COMMAND} -E remove_directory ${UMESIMD_DESTDIR}/unittest
 )
 
 add_custom_target(UMESIMD)
 add_dependencies(UMESIMD ${UMESIMD_PROJECT})
 
-set(UMESIMD_INCLUDE_DIRS ${UMESIMD_DESTDIR}/include CACHE INTERNAL "UMESIMD include directories")
+set(UMESIMD_INCLUDE_DIRS ${UMESIMD_ROOTDIR}/include)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(UMESIMD
   REQUIRED_VARS UMESIMD_INCLUDE_DIRS VERSION_VAR UMESIMD_VERSION)
 
-install(DIRECTORY ${CMAKE_BINARY_DIR}/${UMESIMD_PROJECT}/ DESTINATION ${CMAKE_INSTALL_PREFIX})
+install(DIRECTORY ${UMESIMD_ROOTDIR}/ DESTINATION ${CMAKE_INSTALL_PREFIX})
