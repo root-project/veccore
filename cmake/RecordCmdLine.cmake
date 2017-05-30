efc5c9388bc5462adae715376d2e2497b1d29ea8
Guilherme Amadio <amadio@cern.ch>
2017-05-30 10:39:51 +0200

    Fix coding style of RecordCmdLine.cmake

--- cmake/RecordCmdLine.cmake
+++ cmake/RecordCmdLine.cmake
@@ -1,7 +1,6 @@
 # Try to capture the initial set of cmake command line args passed by
-# the user for configuration.
-# Original Recipe taken from http://stackoverflow.com/questions/10205986/how-to-capture-cmake-command-line-arguments
-# and adapted in ROOT and GeantV
+# the user for configuration. Original Recipe taken from the link below:
+# http://stackoverflow.com/questions/10205986/how-to-capture-cmake-command-line-arguments
 #
 # Note: The entries will live on CMakeCache.txt, so re-configuring with
 # a command line that doesn't include an option won't remove it. You need
@@ -12,39 +11,42 @@
 # The history of tweaks to the configuration (except those from ccmake) is
 # recorded as:
 #     recmake_replay.sh
-#
-GET_CMAKE_PROPERTY(CACHE_VARS CACHE_VARIABLES)
-FOREACH(CACHE_VAR ${CACHE_VARS})
-    GET_PROPERTY(CACHE_VAR_HELPSTRING CACHE ${CACHE_VAR} PROPERTY HELPSTRING)
-    IF(CACHE_VAR_HELPSTRING STREQUAL "No help, variable specified on the command line.")
-        GET_PROPERTY(CACHE_VAR_TYPE CACHE ${CACHE_VAR} PROPERTY TYPE)
-        IF(CACHE_VAR_TYPE STREQUAL "UNINITIALIZED")
-            SET(CACHE_VAR_TYPE)
-        ELSE(CACHE_VAR_TYPE STREQUAL "UNINITIALIZED")
-            SET(CACHE_VAR_TYPE :${CACHE_VAR_TYPE})
-        ENDIF()
-        SET(CMAKE_INVOKE_ARGS "${CMAKE_INVOKE_ARGS} -D${CACHE_VAR}${CACHE_VAR_TYPE}=\"${${CACHE_VAR}}\"")
-    ENDIF()
-ENDFOREACH(CACHE_VAR ${CACHE_VARS})
-# Record the full command line invocation.
-SET(CMAKE_INVOKE "${CMAKE_COMMAND} ${CMAKE_INVOKE_ARGS} ${CMAKE_CURRENT_SOURCE_DIR}" CACHE STRING "Command used to invoke cmake" FORCE)
-# Create a simple shell script that allows us to reinvoke cmake with the captured command line.
-IF (NOT WIN32)
-    if (NOT ${CMAKE_GENERATOR} STREQUAL "Unix Makefiles")
-        set(RECMAKE_GENERATOR "-G ${CMAKE_GENERATOR}")
-    endif()
-    SET(RECMAKE_REPLAY_FILE ${CMAKE_BINARY_DIR}/recmake_replay.sh)
-    SET(RECMAKE_INITIAL_FILE ${CMAKE_BINARY_DIR}/recmake_initial.sh)
-    if (NOT EXISTS ${RECMAKE_INITIAL_FILE})
-        FILE(WRITE ${RECMAKE_INITIAL_FILE} "#!/bin/sh\n"
-                "rm -f CMakeCache.txt\n"
-                "${CMAKE_INVOKE} ${RECMAKE_GENERATOR}\n")
-    endif()
-    if (EXISTS ${RECMAKE_REPLAY_FILE})
-        FILE(APPEND ${RECMAKE_REPLAY_FILE} "${CMAKE_INVOKE}\n")
+
+get_cmake_property(CACHE_VARS CACHE_VARIABLES)
+foreach (CACHE_VAR ${CACHE_VARS})
+  get_property(CACHE_VAR_HELPSTRING CACHE ${CACHE_VAR} PROPERTY HELPSTRING)
+  if (CACHE_VAR_HELPSTRING STREQUAL "No help, variable specified on the command line.")
+    get_property(CACHE_VAR_TYPE CACHE ${CACHE_VAR} PROPERTY TYPE)
+    if (CACHE_VAR_TYPE STREQUAL "UNINITIALIZED")
+      set(CACHE_VAR_TYPE)
     else()
-        FILE(WRITE ${RECMAKE_REPLAY_FILE} "#!/bin/sh\n"
-            "rm -f CMakeCache.txt\n"
-            "${CMAKE_INVOKE} ${RECMAKE_GENERATOR}\n")
+      set(CACHE_VAR_TYPE :${CACHE_VAR_TYPE})
     endif()
-ENDIF (NOT WIN32)
+    set(CMAKE_INVOKE_ARGS "${CMAKE_INVOKE_ARGS} -D${CACHE_VAR}${CACHE_VAR_TYPE}=\"${${CACHE_VAR}}\"")
+  endif()
+endforeach(CACHE_VAR ${CACHE_VARS})
+
+# Record CMake command line
+set(CMAKE_INVOKE "${CMAKE_COMMAND} ${CMAKE_INVOKE_ARGS} ${CMAKE_CURRENT_SOURCE_DIR}"
+  CACHE STRING "Command used to invoke cmake" FORCE)
+
+# Create a shell script that reinvokes CMake with the captured command line
+if (NOT ${CMAKE_GENERATOR} STREQUAL "Unix Makefiles")
+  set(RECMAKE_GENERATOR "-G ${CMAKE_GENERATOR}")
+endif()
+set(RECMAKE_INITIAL_FILE ${CMAKE_BINARY_DIR}/recmake_initial.sh)
+set(RECMAKE_REPLAY_FILE ${CMAKE_BINARY_DIR}/recmake_replay.sh)
+
+if (NOT EXISTS ${RECMAKE_INITIAL_FILE})
+  file(WRITE ${RECMAKE_INITIAL_FILE}
+    "#!/bin/sh\nrm -f CMakeCache.txt\n"
+    "${CMAKE_INVOKE} ${RECMAKE_GENERATOR}\n")
+endif()
+
+if (EXISTS ${RECMAKE_REPLAY_FILE})
+  file(APPEND ${RECMAKE_REPLAY_FILE} "${CMAKE_INVOKE}\n")
+else()
+  file(WRITE ${RECMAKE_REPLAY_FILE}
+    "#!/bin/sh\nrm -f CMakeCache.txt\n"
+    "${CMAKE_INVOKE} ${RECMAKE_GENERATOR}\n")
+endif()
