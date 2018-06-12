eaf188d615bc4ef26e6a7839562880cbbe505e8f
Guilherme Amadio <amadio@cern.ch>
2018-06-12 15:41:13 +0200

    Improve VecCore testing of math functions
    - Enable more functions to be tested
    - Enable testing of math functions from UME::SIMD backend

--- test/math.cc
+++ test/math.cc
@@ -5,8 +5,6 @@
 
 using namespace testing;
 
-#if defined(GTEST_HAS_TYPED_TEST) && defined(GTEST_HAS_TYPED_TEST_P)
-
 template <class Backend>
 using FloatTypes = Types<typename Backend::Float_v, typename Backend::Double_v>;
 
@@ -32,37 +30,30 @@ double uniform_random(double a, double b)
   return a + (b - a) * drand48();
 }
 
-#define TEST_MATH_FUNCTION_RANGE(func, stdfunc, a, b)         \
-  TYPED_TEST_P(MathFunctions, func)                           \
-  {                                                           \
-    using Scalar_t = typename TestFixture::Scalar_t;          \
-    using Vector_t = typename TestFixture::Vector_t;          \
-                                                              \
-    auto kVS = vecCore::VectorSize<Vector_t>();               \
-    size_t N = 2 * kVS;                                       \
-    Scalar_t input[N];                                        \
-    Scalar_t output[N];                                       \
-                                                              \
-    for (size_t i = 0; i < N; ++i) {                          \
-      input[i] = static_cast<Scalar_t>(uniform_random(a, b)); \
-    }                                                         \
-                                                              \
-    for (size_t j = 0; j < N; j += kVS) {                     \
-      Vector_t x(vecCore::FromPtr<Vector_t>(&input[j]));      \
-      Vector_t y = vecCore::math::func(x);                    \
-      vecCore::Store<Vector_t>(y, &output[j]);                \
-    }                                                         \
-                                                              \
-    for (size_t i = 0; i < N; ++i) {                          \
-      EXPECT_FLOAT_EQ(output[i], std::stdfunc(input[i]));     \
-    }                                                         \
+#define TEST_MATH_FUNCTION_RANGE(func, stdfunc, a, b)                 \
+  TYPED_TEST_P(MathFunctions, func)                                   \
+  {                                                                   \
+    using Scalar_t = typename TestFixture::Scalar_t;                  \
+    using Vector_t = typename TestFixture::Vector_t;                  \
+                                                                      \
+    auto kVS = vecCore::VectorSize<Vector_t>();                       \
+    size_t N = 16384;                                                 \
+    Scalar_t input[N]  __attribute__((aligned(VECCORE_SIMD_ALIGN)));  \
+    Scalar_t output[N] __attribute__((aligned(VECCORE_SIMD_ALIGN)));  \
+                                                                      \
+    for (size_t i = 0; i < N; ++i)                                    \
+      input[i] = static_cast<Scalar_t>(uniform_random(a, b));         \
+                                                                      \
+    for (size_t j = 0; j < N; j += kVS) {                             \
+      Vector_t x;                                                     \
+      vecCore::Load(x, &input[j]);                                    \
+      vecCore::Store<Vector_t>(vecCore::math::func(x), &output[j]);   \
+    }                                                                 \
+                                                                      \
+    for (size_t i = 0; i < N; ++i) {                                  \
+      EXPECT_FLOAT_EQ(std::stdfunc(input[i]), output[i]);             \
+    }                                                                 \
   }
-
-#define TEST_MATH_FUNCTION(F, f) TEST_MATH_FUNCTION_RANGE(F, f, FLT_MIN, FLT_MAX)
-#define TEST_MATH_FUNCTION_ONE(F, f) TEST_MATH_FUNCTION_RANGE(F, f, -1.0, 1.0)
-#define TEST_MATH_FUNCTION_NEG(F, f) TEST_MATH_FUNCTION_RANGE(F, f, -1.0, 0.0)
-#define TEST_MATH_FUNCTION_POS(F, f) TEST_MATH_FUNCTION_RANGE(F, f, 0.0, 1.0)
-#define TEST_MATH_FUNCTION_TRIG(F, f) TEST_MATH_FUNCTION_RANGE(F, f, 0.0, 2.0 * M_PI)
 
 #define TEST_MATH_FUNCTION_RANGE_2(func, stdfunc, a, b, c, d)         \
   TYPED_TEST_P(MathFunctions, func)                                   \
@@ -71,10 +62,10 @@ double uniform_random(double a, double b)
     using Vector_t = typename TestFixture::Vector_t;                  \
                                                                       \
     auto kVS = vecCore::VectorSize<Vector_t>();                       \
-    size_t N = 2 * kVS;                                               \
-    Scalar_t input1[N];                                               \
-    Scalar_t input2[N];                                               \
-    Scalar_t output[N];                                               \
+    size_t N = 16384;                                                 \
+    Scalar_t input1[N] __attribute__((aligned(VECCORE_SIMD_ALIGN)));  \
+    Scalar_t input2[N] __attribute__((aligned(VECCORE_SIMD_ALIGN)));  \
+    Scalar_t output[N] __attribute__((aligned(VECCORE_SIMD_ALIGN)));  \
                                                                       \
     for (size_t i = 0; i < N; ++i) {                                  \
       input1[i] = static_cast<Scalar_t>(uniform_random(a, b));        \
@@ -82,10 +73,10 @@ double uniform_random(double a, double b)
     }                                                                 \
                                                                       \
     for (size_t j = 0; j < N; j += kVS) {                             \
-      Vector_t x(vecCore::FromPtr<Vector_t>(&input1[j]));             \
-      Vector_t y(vecCore::FromPtr<Vector_t>(&input2[j]));             \
-      Vector_t r = vecCore::math::func(x, y);                         \
-      vecCore::Store<Vector_t>(r, &output[j]);                        \
+      Vector_t x, y;                                                  \
+      vecCore::Load(x, &input1[j]);                                   \
+      vecCore::Load(y, &input2[j]);                                   \
+      vecCore::Store<Vector_t>(vecCore::math::func(x,y), &output[j]); \
     }                                                                 \
                                                                       \
     for (size_t i = 0; i < N; ++i) {                                  \
@@ -93,23 +84,33 @@ double uniform_random(double a, double b)
     }                                                                 \
   }
 
-#define TEST_MATH_FUNCTION_2(F, f) TEST_MATH_FUNCTION_RANGE_2(F, f, FLT_MIN, FLT_MAX, FLT_MIN, FLT_MAX)
 
-// commented functions are not yet implemented in Vc, need to be implemented in VecCore
+#define TEST_MATH_FUNCTION(F, f) TEST_MATH_FUNCTION_RANGE(F, f, -FLT_MAX, FLT_MAX)
+#define TEST_MATH_FUNCTION_ONE(F, f) TEST_MATH_FUNCTION_RANGE(F, f, -1.0, 1.0)
+#define TEST_MATH_FUNCTION_NEG(F, f) TEST_MATH_FUNCTION_RANGE(F, f, -1.0, 0.0)
+#define TEST_MATH_FUNCTION_POS(F, f) TEST_MATH_FUNCTION_RANGE(F, f, 0.0, 1.0)
+#define TEST_MATH_FUNCTION_TRIG(F, f) TEST_MATH_FUNCTION_RANGE(F, f, 0.0, 2.0 * M_PI)
+
+#define TEST_MATH_FUNCTION_2(F, f) TEST_MATH_FUNCTION_RANGE_2(F, f, -FLT_MAX, FLT_MAX, -FLT_MAX, FLT_MAX)
 
 TEST_MATH_FUNCTION(Abs, abs);
 TEST_MATH_FUNCTION(Floor, floor);
 TEST_MATH_FUNCTION(Ceil, ceil);
+
 TEST_MATH_FUNCTION_TRIG(Sin, sin);
-TEST_MATH_FUNCTION_ONE(ASin, asin);
-// TEST_MATH_FUNCTION(Sinh, sinh);
-// TEST_MATH_FUNCTION(ASinh, asinh);
 TEST_MATH_FUNCTION_TRIG(Cos, cos);
-// TEST_MATH_FUNCTION(ACos, acos);
-// TEST_MATH_FUNCTION(Cosh, cosh);
-// TEST_MATH_FUNCTION(ACosh, acosh);
 TEST_MATH_FUNCTION_TRIG(Tan, tan);
+TEST_MATH_FUNCTION_ONE(ASin, asin);
+TEST_MATH_FUNCTION_ONE(ACos, acos);
 TEST_MATH_FUNCTION(ATan, atan);
+
+TEST_MATH_FUNCTION(Sinh, sinh);
+TEST_MATH_FUNCTION(Cosh, cosh);
+TEST_MATH_FUNCTION(Tanh, tanh);
+TEST_MATH_FUNCTION(ASinh, asinh);
+TEST_MATH_FUNCTION_RANGE(ACosh, acosh,  1., 100.);
+TEST_MATH_FUNCTION_ONE(ATanh, atanh);
+
 TEST_MATH_FUNCTION(Exp, exp);
 TEST_MATH_FUNCTION_POS(Log, log);
 TEST_MATH_FUNCTION_POS(Sqrt, sqrt);
@@ -118,12 +119,17 @@ TEST_MATH_FUNCTION(Trunc, trunc);
 
 TEST_MATH_FUNCTION_2(ATan2, atan2);
 TEST_MATH_FUNCTION_2(CopySign, copysign);
-TEST_MATH_FUNCTION_2(Pow, pow);
 
-REGISTER_TYPED_TEST_CASE_P(MathFunctions, Abs, Floor, Ceil, Sin, ASin, Cos, Tan, ATan, Exp, Log, Sqrt, Cbrt, Trunc, ATan2, CopySign,
-                           Pow);
+TEST_MATH_FUNCTION_RANGE_2(Pow, pow, FLT_MIN, FLT_MAX, 1.0, 10.0);
 
-#define TEST_BACKEND_P(name, x) INSTANTIATE_TYPED_TEST_CASE_P(name, MathFunctions, FloatTypes<vecCore::backend::x>);
+REGISTER_TYPED_TEST_CASE_P(MathFunctions,
+   Abs, CopySign, Floor, Ceil,
+   Sin, Cos, Tan, ASin, ACos, ATan, ATan2,
+   Sinh, Cosh, Tanh, ASinh, ACosh, ATanh,
+   Exp, Log, Sqrt, Cbrt, Pow, Trunc);
+
+#define TEST_BACKEND_P(name, x) \
+  INSTANTIATE_TYPED_TEST_CASE_P(name, MathFunctions, FloatTypes<vecCore::backend::x>);
 
 #define TEST_BACKEND(x) TEST_BACKEND_P(x, x)
 
@@ -132,16 +138,15 @@ REGISTER_TYPED_TEST_CASE_P(MathFunctions, Abs, Floor, Ceil, Sin, ASin, Cos, Tan,
 TEST_BACKEND(Scalar);
 TEST_BACKEND(ScalarWrapper);
 
+#ifdef VECCORE_ENABLE_UMESIMD
+TEST_BACKEND(UMESimd);
+TEST_BACKEND_P(UMESimdArray, UMESimdArray<16>);
+#endif
+
 #ifdef VECCORE_ENABLE_VC
 TEST_BACKEND(VcScalar);
 TEST_BACKEND(VcVector);
 TEST_BACKEND_P(VcSimdArray, VcSimdArray<16>);
-#endif
-
-#else // if !GTEST_HAS_TYPED_TEST
-TEST(DummyTest, TypedTestsAreNotSupportedOnThisPlatform)
-{
-}
 #endif
 
 int main(int argc, char *argv[])
