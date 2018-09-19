6464fdecc4a7a840041c1a63835154536708d9c6
Guilherme Amadio <amadio@cern.ch>
2018-09-19 10:54:50 +0200

    Mark numeric limits functions as noexcept

--- include/VecCore/Limits.h
+++ include/VecCore/Limits.h
@@ -25,107 +25,107 @@ struct NumericLimits {
 
 template <>
 struct NumericLimits<bool> {
-  VECCORE_ATT_HOST_DEVICE static constexpr bool Min() { return false; }
-  VECCORE_ATT_HOST_DEVICE static constexpr bool Max() { return true; }
+  VECCORE_ATT_HOST_DEVICE static constexpr bool Min() noexcept { return false; }
+  VECCORE_ATT_HOST_DEVICE static constexpr bool Max() noexcept { return true; }
 };
 
 template <>
 struct NumericLimits<char> {
-  VECCORE_ATT_HOST_DEVICE static constexpr char Min() { return CHAR_MIN; }
-  VECCORE_ATT_HOST_DEVICE static constexpr char Max() { return CHAR_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr char Lowest() { return CHAR_MIN; }
-  VECCORE_ATT_HOST_DEVICE static constexpr char Highest() { return CHAR_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr char Min() noexcept { return CHAR_MIN; }
+  VECCORE_ATT_HOST_DEVICE static constexpr char Max() noexcept { return CHAR_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr char Lowest() noexcept { return CHAR_MIN; }
+  VECCORE_ATT_HOST_DEVICE static constexpr char Highest() noexcept { return CHAR_MAX; }
 };
 
 template <>
 struct NumericLimits<unsigned char> {
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned char Min() { return 0; }
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned char Max() { return UCHAR_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned char Lowest() { return 0; }
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned char Highest() { return UCHAR_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned char Min() noexcept { return 0; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned char Max() noexcept { return UCHAR_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned char Lowest() noexcept { return 0; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned char Highest() noexcept { return UCHAR_MAX; }
 };
 
 template <>
 struct NumericLimits<short> {
-  VECCORE_ATT_HOST_DEVICE static constexpr short Min() { return SHRT_MIN; }
-  VECCORE_ATT_HOST_DEVICE static constexpr short Max() { return SHRT_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr short Lowest() { return SHRT_MIN; }
-  VECCORE_ATT_HOST_DEVICE static constexpr short Highest() { return SHRT_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr short Min() noexcept { return SHRT_MIN; }
+  VECCORE_ATT_HOST_DEVICE static constexpr short Max() noexcept { return SHRT_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr short Lowest() noexcept { return SHRT_MIN; }
+  VECCORE_ATT_HOST_DEVICE static constexpr short Highest() noexcept { return SHRT_MAX; }
 };
 
 template <>
 struct NumericLimits<unsigned short> {
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned short Min() { return 0; }
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned short Max() { return USHRT_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned short Lowest() { return 0; }
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned short Highest() { return USHRT_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned short Min() noexcept { return 0; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned short Max() noexcept { return USHRT_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned short Lowest() noexcept { return 0; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned short Highest() noexcept { return USHRT_MAX; }
 };
 
 template <>
 struct NumericLimits<int> {
-  VECCORE_ATT_HOST_DEVICE static constexpr int Min() { return INT_MIN; }
-  VECCORE_ATT_HOST_DEVICE static constexpr int Max() { return INT_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr int Lowest() { return INT_MIN; }
-  VECCORE_ATT_HOST_DEVICE static constexpr int Highest() { return INT_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr int Min() noexcept { return INT_MIN; }
+  VECCORE_ATT_HOST_DEVICE static constexpr int Max() noexcept { return INT_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr int Lowest() noexcept { return INT_MIN; }
+  VECCORE_ATT_HOST_DEVICE static constexpr int Highest() noexcept { return INT_MAX; }
 };
 
 template <>
 struct NumericLimits<unsigned int> {
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned int Min() { return 0; }
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned int Max() { return UINT_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned int Lowest() { return 0; }
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned int Highest() { return UINT_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned int Min() noexcept { return 0; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned int Max() noexcept { return UINT_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned int Lowest() noexcept { return 0; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned int Highest() noexcept { return UINT_MAX; }
 };
 
 template <>
 struct NumericLimits<long> {
-  VECCORE_ATT_HOST_DEVICE static constexpr long Min() { return LONG_MIN; }
-  VECCORE_ATT_HOST_DEVICE static constexpr long Max() { return LONG_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr long Lowest() { return LONG_MIN; }
-  VECCORE_ATT_HOST_DEVICE static constexpr long Highest() { return LONG_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr long Min() noexcept { return LONG_MIN; }
+  VECCORE_ATT_HOST_DEVICE static constexpr long Max() noexcept { return LONG_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr long Lowest() noexcept { return LONG_MIN; }
+  VECCORE_ATT_HOST_DEVICE static constexpr long Highest() noexcept { return LONG_MAX; }
 };
 
 template <>
 struct NumericLimits<unsigned long> {
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long Min() { return 0UL; }
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long Max() { return ULONG_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long Lowest() { return 0UL; }
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long Highest() { return ULONG_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long Min() noexcept { return 0UL; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long Max() noexcept { return ULONG_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long Lowest() noexcept { return 0UL; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long Highest() noexcept { return ULONG_MAX; }
 };
 
 template <>
 struct NumericLimits<long long> {
-  VECCORE_ATT_HOST_DEVICE static constexpr long long Min() { return LLONG_MIN; }
-  VECCORE_ATT_HOST_DEVICE static constexpr long long Max() { return LLONG_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr long long Lowest() { return LLONG_MIN; }
-  VECCORE_ATT_HOST_DEVICE static constexpr long long Highest() { return LLONG_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr long long Min() noexcept { return LLONG_MIN; }
+  VECCORE_ATT_HOST_DEVICE static constexpr long long Max() noexcept { return LLONG_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr long long Lowest() noexcept { return LLONG_MIN; }
+  VECCORE_ATT_HOST_DEVICE static constexpr long long Highest() noexcept { return LLONG_MAX; }
 };
 
 template <>
 struct NumericLimits<unsigned long long> {
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long long Min() { return 0ULL; }
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long long Max() { return ULLONG_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long long Lowest() { return 0ULL; }
-  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long long Highest() { return ULLONG_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long long Min() noexcept { return 0ULL; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long long Max() noexcept { return ULLONG_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long long Lowest() noexcept { return 0ULL; }
+  VECCORE_ATT_HOST_DEVICE static constexpr unsigned long long Highest() noexcept { return ULLONG_MAX; }
 };
 
 template <>
 struct NumericLimits<float> {
-  VECCORE_ATT_HOST_DEVICE static constexpr float Min() { return FLT_MIN; }
-  VECCORE_ATT_HOST_DEVICE static constexpr float Max() { return FLT_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr float Lowest() { return -FLT_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr float Highest() { return FLT_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr float Epsilon() { return FLT_EPSILON; }
+  VECCORE_ATT_HOST_DEVICE static constexpr float Min() noexcept { return FLT_MIN; }
+  VECCORE_ATT_HOST_DEVICE static constexpr float Max() noexcept { return FLT_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr float Lowest() noexcept { return -FLT_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr float Highest() noexcept { return FLT_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr float Epsilon() noexcept { return FLT_EPSILON; }
   VECCORE_ATT_HOST_DEVICE static constexpr float Infinity() noexcept { return HUGE_VALF; }
 };
 
 template <>
 struct NumericLimits<double> {
-  VECCORE_ATT_HOST_DEVICE static constexpr double Min() { return DBL_MIN; }
-  VECCORE_ATT_HOST_DEVICE static constexpr double Max() { return DBL_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr double Lowest() { return -DBL_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr double Highest() { return DBL_MAX; }
-  VECCORE_ATT_HOST_DEVICE static constexpr double Epsilon() { return DBL_EPSILON; }
+  VECCORE_ATT_HOST_DEVICE static constexpr double Min() noexcept { return DBL_MIN; }
+  VECCORE_ATT_HOST_DEVICE static constexpr double Max() noexcept { return DBL_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr double Lowest() noexcept { return -DBL_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr double Highest() noexcept { return DBL_MAX; }
+  VECCORE_ATT_HOST_DEVICE static constexpr double Epsilon() noexcept { return DBL_EPSILON; }
   VECCORE_ATT_HOST_DEVICE static constexpr double Infinity() noexcept { return HUGE_VAL; }
 };
 
