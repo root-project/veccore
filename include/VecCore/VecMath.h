8d3753dc6d038bbb256d1a89bc93303e255ecada
Guilherme Amadio <amadio@cern.ch>
2019-05-22 09:49:34 +0200

    Add sincosf()/sincos() implementations for Windows

--- include/VecCore/VecMath.h
+++ include/VecCore/VecMath.h
@@ -15,6 +15,20 @@ void sincos(const double &x, double *s, double *c)
 {
   __sincos(x, s, c);
 }
+#elif defined(_MSC_VER)
+VECCORE_FORCE_INLINE
+void sincosf(const float &x, float *s, float *c)
+{
+  *s = sinf(x);
+  *c = cosf(x);
+}
+
+VECCORE_FORCE_INLINE
+void sincos(const double &x, double *s, double *c)
+{
+  *s = sin(x);
+  *c = cos(x);
+}
 #endif
 
 namespace vecCore {
