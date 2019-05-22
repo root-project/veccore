918dee47bea9d9af71b37d750b31448911880b25
Guilherme Amadio <amadio@cern.ch>
2019-05-22 09:51:35 +0200

    Add AlignedAlloc()/AlignedFree() implementations for Windows

--- include/VecCore/Utilities.h
+++ include/VecCore/Utilities.h
@@ -1,6 +1,8 @@
 #ifndef VECCORE_UTILITIES_H
 #define VECCORE_UTILITIES_H
 
+#ifndef _MSC_VER
+
 #include <cstdlib>
 
 namespace vecCore {
@@ -10,7 +12,7 @@ inline void *AlignedAlloc(size_t alignment, size_t size)
   void *ptr = nullptr;
 
   if (posix_memalign(&ptr, alignment, size) == 0)
-     return ptr;
+    return ptr;
 
   return nullptr;
 }
@@ -19,6 +21,27 @@ inline void AlignedFree(void *ptr)
 {
   free(ptr);
 }
+
 }
 
-#endif
+#else // _MSC_VER
+
+#include <malloc.h>
+
+namespace vecCore {
+
+inline void *AlignedAlloc(size_t alignment, size_t size)
+{
+  return _aligned_malloc(size, alignment);
+}
+
+inline void AlignedFree(void *ptr)
+{
+  _aligned_free(ptr);
+}
+
+}
+
+#endif // _MSC_VER
+
+#endif // VECCORE_UTILITIES_H
