4e47d85320d733671872596fc877304135c215c5
Guilherme Amadio <amadio@cern.ch>
2019-05-22 09:47:53 +0200

    Add VECCORE_FORCE_{INLINE,NOINLINE} implementations for Windows

--- include/VecCore/Common.h
+++ include/VecCore/Common.h
@@ -5,8 +5,13 @@
 #include "SIMD.h"
 
 #ifndef VECCORE_NO_FORCED_INLINING
+#ifdef _MSC_VER
+#define VECCORE_FORCE_NOINLINE __declspec(noinline)
+#define VECCORE_FORCE_INLINE inline __forceinline
+#else
 #define VECCORE_FORCE_NOINLINE __attribute__((noinline))
 #define VECCORE_FORCE_INLINE inline __attribute__((always_inline))
+#endif
 #else
 #define VECCORE_FORCE_NOINLINE
 #define VECCORE_FORCE_INLINE
