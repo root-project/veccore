d426f76b670d1eee7a693f0f9083d22c4735e90d
Guilherme Amadio <amadio@cern.ch>
2019-07-04 16:05:06 +0200

    Define macros for supported architectures

--- include/VecCore/Common.h
+++ include/VecCore/Common.h
@@ -1,6 +1,18 @@
 #ifndef VECCORE_COMMON_H
 #define VECCORE_COMMON_H
 
+#if defined(__x86_64__) || defined(_M_X64)
+#define VECCORE_X64
+#elif defined(__i386__) || defined(_M_IX86)
+#define VECCORE_X86
+#elif defined(__aarch64__) || defined(_M_ARM64)
+#define VECCORE_ARM64
+#elif defined(__arm__) || defined(_M_ARM)
+#define VECCORE_ARM
+#elif defined(__PPC64__)
+#define VECCORE_PPC64
+#endif
+
 #include "CUDA.h"
 #include "SIMD.h"
 
