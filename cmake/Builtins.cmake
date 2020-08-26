29b10e15017933fc75b36994d6ad2c6395655817
Ben Morgan <Ben.Morgan@warwick.ac.uk>
2020-08-26 15:16:52 +0100

    Improve support for subproject use and builtins
    Client projects may use VecCore as a subproject, i.e. by adding
    it to their CMake build through an add_subdirectory call. They may
    also use find_package to locate an external install.
    
    To support both use cases transparently, create VecCore::VecCore
    IMPORTED target alongside VecCore. The target is reconstructed in
    the same way as is done in VecCoreConfig to configure the requested
    backend. It is made GLOBAL as client projects will always include
    VecCore in a subdirectory. Client projects can link to VecCore::VecCore
    independent of using VecCore as a subproject or external package.
    
    The canonical ALIAS target method is not used as this requires export
    of the VecCore target. This exports any build time choise of backend
    as a dependency of the imported target, making configuration/use more
    difficult.
    
    VecCore can support build of optional backends like UMESIMD and Vc.
    Simplify the use of builtin UMESIMD by adding it as a subproject using
    FetchContent rather than ExternalProject. This structure is cleaner and
    less prone to ordering/dependency issues.
    
    This latter change requires a bump in CMake version to 3.11 to provide
    FetchContent. A bump to CMake 3.12 has been made as this provides a
    greater range of features, especially awareness of C++20.

--- cmake/Builtins.cmake
+++ cmake/Builtins.cmake
@@ -1,6 +1,8 @@
+include(FetchContent)
+
 macro(clear_var var)
-    unset(${var})
-    unset(${var} CACHE)
+  unset(${var})
+  unset(${var} CACHE)
 endmacro()
 
 macro(clear_pkg_vars pkg)
@@ -17,7 +19,7 @@ endmacro()
 
 if(BUILD_VC)
   list(APPEND BUILTINS Vc)
-  add_subdirectory(builtins/Vc)
+  add_subdirectory(cmake/Vc)
   set(VC ON CACHE BOOL "" FORCE)
 else()
   clear_pkg_vars(Vc)
@@ -25,7 +27,31 @@ endif()
 
 if(BUILD_UMESIMD)
   list(APPEND BUILTINS UMESIMD)
-  add_subdirectory(builtins/umesimd)
+
+  FetchContent_Declare(umesimd
+    URL "http://lcgpackages.web.cern.ch/lcgpackages/tarFiles/sources/umesimd-0.8.1.tar.gz"
+    URL_HASH MD5=3960a11a20a4294e68480ab0d39db6d7
+    SOURCE_DIR ${FETCHCONTENT_BASE_DIR}/umesimd)
+
+  FetchContent_GetProperties(umesimd)
+  if(NOT umesimd_POPULATED)
+    FetchContent_Populate(umesimd)
+  endif()
+
+  add_library(UMESIMD::UMESIMD INTERFACE IMPORTED)
+  set_target_properties(UMESIMD::UMESIMD PROPERTIES
+    INTERFACE_INCLUDE_DIRECTORIES ${FETCHCONTENT_BASE_DIR})
+
+  # Awkward manual install due to UMESIMD's bad packaging
+  install(DIRECTORY ${umesimd_SOURCE_DIR}/
+    DESTINATION ${CMAKE_INSTALL_INCLUDEDIR}/umesimd
+    PATTERN "CMakeLists.txt" EXCLUDE
+    PATTERN "README.md" EXCLUDE
+    PATTERN "doc" EXCLUDE
+    PATTERN "examples" EXCLUDE
+    PATTERN "microbenchmarks" EXCLUDE
+    PATTERN "unittest" EXCLUDE)
+
   set(UMESIMD ON CACHE BOOL "" FORCE)
 else()
   clear_pkg_vars(UMESIMD)
