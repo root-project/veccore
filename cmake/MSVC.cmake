c81bdc75f5b64d5b83e456f97330be957f5a6fbd
Guilherme Amadio <amadio@cern.ch>
2021-03-02 16:36:19 +0100

    Disable annoying type conversion warnings from MSVC
    We know the conversions we make in tests are safe and do not
    cause any loss of data.

--- cmake/MSVC.cmake
+++ cmake/MSVC.cmake
@@ -2,6 +2,10 @@
 # Note: MSVC does not have an option equivalent to -march=native
 # https://docs.microsoft.com/en-us/cpp/build/reference/arch-x86?view=vs-2019
 
+# Disable annoying warnings about unsafe type conversions and MSVC's headers.
+# We know the type conversions we do in the tests are safe.
+add_compile_options(/wd4244 /wd4267 /wd4305 /wd4756)
+
 set(FLAGS_SSE    "/arch:SSE")
 set(FLAGS_SSE2   "/arch:SSE2")
 set(FLAGS_AVX    "/arch:AVX")
