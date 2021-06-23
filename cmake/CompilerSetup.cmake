cb5d56662cfa7ae00535708fd4daa9e2424334dd
Guilherme Amadio <amadio@cern.ch>
2021-06-23 18:06:50 +0200

    Ignore TARGET_ISA if set to an empty string

--- cmake/CompilerSetup.cmake
+++ cmake/CompilerSetup.cmake
@@ -4,7 +4,7 @@ else()
   message(WARNING "Unsupported compiler: ${CMAKE_CXX_COMPILER_ID}")
 endif()
 
-if (DEFINED TARGET_ISA)
+if (DEFINED TARGET_ISA AND NOT "${TARGET_ISA}" STREQUAL "")
   string(TOUPPER "${TARGET_ISA}" TARGET_ISA)
 
   if (NOT "${TARGET_ISA}" MATCHES "^(NATIVE|SSE(2|3|4.(1|2))|AVX(|2|512)|KNC|KNL)$")
