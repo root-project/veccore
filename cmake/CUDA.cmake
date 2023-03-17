584e32a1c41e9607a754b262899600c360083949
Guilherme Amadio <amadio@cern.ch>
2023-03-17 11:01:23 +0100

    Drop check for C++ standard in CUDA.cmake
    CUDA 11 does support C++17 for CUDA code, and CUDA 11.7 adds
    some support for C++20, so this check has become obsolete.

--- cmake/CUDA.cmake
+++ cmake/CUDA.cmake
@@ -1,7 +1,3 @@
 message(STATUS "Compiling with CUDA enabled")
 
 find_package(CUDA 8.0 REQUIRED)
-
-if(CMAKE_CXX_STANDARD VERSION_GREATER_EQUAL 17)
-  message(FATAL_ERROR "CUDA compilation supports only up to ISO C++14 standard")
-endif()
